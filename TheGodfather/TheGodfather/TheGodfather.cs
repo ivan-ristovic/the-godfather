1b7904f0c37db371262b30297fd909dc8e54f8ee
Ivan Ristović <ivo.ristovic@gmail.com>
2017-08-30 22:19:39 +0200

    DM ignoring and logging

--- TheGodfather/TheGodfather/TheGodfather.cs
+++ TheGodfather/TheGodfather/TheGodfather.cs
@@ -137,8 +137,11 @@ namespace TheGodfatherBot
 
         private Task Client_MessageCreated(MessageCreateEventArgs e)
         {
-            if (e.Message.Author.IsBot || e.Channel.IsPrivate) {
-                e.Client.DebugLogger.LogMessage(LogLevel.Info, "TheGodfather", $"IGNORED: {e.Author.Username} : {e.Message}", DateTime.Now);
+            if (e.Message.Author.IsBot)
+                return Task.CompletedTask;
+
+            if (e.Channel.IsPrivate) {
+                e.Client.DebugLogger.LogMessage(LogLevel.Info, "TheGodfather", $"IGNORED DM: {e.Author.Username} : {e.Message}", DateTime.Now);
                 return Task.CompletedTask;
             }
 
