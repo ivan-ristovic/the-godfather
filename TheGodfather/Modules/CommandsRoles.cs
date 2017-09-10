17cceb5d5c3cb12f60c45e0af38e1b5e712c0bb9
Ivan Ristovic <ivo.ristovic@gmail.com>
2017-09-10 17:05:08 +0200

    Roles

---
+++ TheGodfather/Modules/CommandsRoles.cs
@@ -0,0 +1,31 @@
+﻿#region USING_DIRECTIVES
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+#endregion
+
+namespace TheGodfatherBot
+{
+    [Group("roles", CanInvokeWithoutSubcommand = true)]
+    [Description("Miscellaneous role control commands.")]
+    [Aliases("role", "r", "rl")]
+    [RequirePermissions(Permissions.ManageRoles)]
+    public class CommandsRoles
+    {
+        public async Task ExecuteGroupAsync(CommandContext ctx)
+        {
+            var embed = new DiscordEmbedBuilder() { Title = "Roles:" };
+            foreach (var role in ctx.Guild.Roles)
+                embed.AddField(role.Name, role.Position.ToString(), inline: true);
+            await ctx.RespondAsync("", embed: embed);
+        }
+
+
+    }
+}
--- TheGodfather/TheGodfather.cs
+++ TheGodfather/TheGodfather.cs
@@ -119,6 +119,7 @@ namespace TheGodfatherBot
             _commands.RegisterCommands<CommandsMisc>();
             _commands.RegisterCommands<CommandsRanking>();
             //_commands.RegisterCommands<CommandsReddit>();
+            _commands.RegisterCommands<CommandsRoles>();
             _commands.RegisterCommands<CommandsRSS>();
             _commands.RegisterCommands<CommandsSwat>();
             _commands.RegisterCommands<CommandsUsers>();
