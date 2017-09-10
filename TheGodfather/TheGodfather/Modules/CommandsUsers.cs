568d8cf0341b4c9b8a6369512818880394c74be2
Ivan Ristovic <ivo.ristovic@gmail.com>
2017-09-10 09:46:31 +0200

    User ban

--- TheGodfather/TheGodfather/Modules/CommandsUsers.cs
+++ TheGodfather/TheGodfather/Modules/CommandsUsers.cs
@@ -19,13 +19,28 @@ namespace TheGodfatherBot
         #region COMMAND_USER_KICK
         [Command("kick")]
         [Description("Kicks the user from server.")]
+        [Aliases("k")]
         [RequirePermissions(Permissions.KickMembers)]
-        public async Task List(CommandContext ctx, [Description("User")] DiscordMember u = null)
+        public async Task Kick(CommandContext ctx, [Description("User")] DiscordMember u = null)
         {
             if (u == null)
                 throw new ArgumentNullException("You need to mention a user to kick.");
 
             await ctx.Guild.RemoveMemberAsync(u);
+        }
+        #endregion
+
+        #region COMMAND_USER_BAN
+        [Command("ban")]
+        [Description("Bans the user from server.")]
+        [Aliases("b")]
+        [RequirePermissions(Permissions.KickMembers)]
+        public async Task Ban(CommandContext ctx, [Description("User")] DiscordMember u = null)
+        {
+            if (u == null)
+                throw new ArgumentNullException("You need to mention a user to ban.");
+
+            await ctx.Guild.BanMemberAsync(u);
         }
         #endregion
     }
