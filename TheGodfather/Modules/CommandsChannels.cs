a3ddcfb9ca8784a71f2f71c493651734925219d1
Ivan Ristovic <ivo.ristovic@gmail.com>
2017-09-10 20:38:28 +0200

    Channel create voice

--- TheGodfather/Modules/CommandsChannels.cs
+++ TheGodfather/Modules/CommandsChannels.cs
@@ -19,10 +19,10 @@ namespace TheGodfatherBot
     public class CommandsChannels
     {
         #region COMMAND_CHANNEL_CREATE
-        [Command("create")]
+        [Command("createtxt")]
         [Description("Create new txt channel.")]
-        [Aliases("+", "make", "new", "add")]
-        public async Task CreateChannel(CommandContext ctx, [Description("Name")] string name = null)
+        [Aliases("+t", "make", "new", "add")]
+        public async Task CreateTextChannel(CommandContext ctx, [Description("Name")] string name = null)
         {
             if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentException("Missing channel name.");
@@ -30,6 +30,18 @@ namespace TheGodfatherBot
             await ctx.Guild.CreateChannelAsync(name, ChannelType.Text);
         }
         #endregion
-        
+
+        #region COMMAND_CHANNEL_CREATEVOICE
+        [Command("createvoice")]
+        [Description("Create new voice channel.")]
+        [Aliases("+v", "makev", "newv", "addv")]
+        public async Task CreateVoiceChannel(CommandContext ctx, [Description("Name")] string name = null)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Missing channel name.");
+
+            await ctx.Guild.CreateChannelAsync(name, ChannelType.Voice);
+        }
+        #endregion
     }
 }
