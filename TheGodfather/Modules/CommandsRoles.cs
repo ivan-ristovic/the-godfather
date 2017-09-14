235eb938e19d56aa6e357ff506bd5092c4cde155
Ivan Ristovic <ivo.ristovic@gmail.com>
2017-09-14 21:51:44 +0200

    Roles setmentionable; Update role listing

--- TheGodfather/Modules/CommandsRoles.cs
+++ TheGodfather/Modules/CommandsRoles.cs
@@ -1,6 +1,6 @@
 ﻿#region USING_DIRECTIVES
 using System;
-using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -20,17 +20,21 @@ namespace TheGodfatherBot
     {
         public async Task ExecuteGroupAsync(CommandContext ctx)
         {
-            var embed = new DiscordEmbedBuilder() { Title = "Roles:" };
-            foreach (var role in ctx.Guild.Roles)
-                embed.AddField(role.Name, role.Position.ToString(), inline: true);
-            await ctx.RespondAsync("", embed: embed);
+            string s = "";
+            foreach (var role in ctx.Guild.Roles.OrderBy(r => r.Position).Reverse())
+                s += role.Name + "\n";
+            await ctx.RespondAsync("", embed: new DiscordEmbedBuilder() {
+                Title = "Roles:",
+                Description = s,
+                Color = DiscordColor.Gold
+            });
         }
 
         #region COMMAND_ROLES_CREATE
         [Command("create")]
         [Description("Create a new role.")]
         [Aliases("new", "add", "+")]
-        public async Task CreateRole(CommandContext ctx, [Description("Role name")] string name = null)
+        public async Task CreateRole(CommandContext ctx, [Description("Role")] string name = null)
         {
             if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentException("Missing role name.");
@@ -44,7 +48,7 @@ namespace TheGodfatherBot
         [Command("delete")]
         [Description("Create a new role.")]
         [Aliases("del", "remove", "d", "-")]
-        public async Task DeleteRole(CommandContext ctx, [Description("Role (mention)")] DiscordRole role = null)
+        public async Task DeleteRole(CommandContext ctx, [Description("Role")] DiscordRole role = null)
         {
             if (role == null)
                 throw new ArgumentException("Unknown role.");
@@ -59,7 +63,7 @@ namespace TheGodfatherBot
         [Description("Set a color for the role.")]
         [Aliases("clr", "c")]
         public async Task SetColor(CommandContext ctx, 
-                                  [Description("Role name")] DiscordRole role = null,
+                                  [Description("Role")] DiscordRole role = null,
                                   [Description("Color")] string color = null)
         {
             if (role == null || string.IsNullOrWhiteSpace(color))
@@ -73,7 +77,7 @@ namespace TheGodfatherBot
         #region COMMAND_ROLES_SETNAME
         [Command("setname")]
         [Description("Set a name for the role.")]
-        [Aliases("name", "rename")]
+        [Aliases("name", "rename", "n")]
         public async Task RenameRole(CommandContext ctx,
                                     [Description("Role")] DiscordRole role = null,
                                     [Description("New name")] string name = null)
@@ -83,6 +87,22 @@ namespace TheGodfatherBot
 
             await ctx.Guild.UpdateRoleAsync(role, name: name);
             await ctx.RespondAsync("Successfully changed role name.");
+        }
+        #endregion
+
+        #region COMMAND_ROLES_SETMENTIONABLE
+        [Command("setmentionable")]
+        [Description("Role modification swiss-key.")]
+        [Aliases("mentionable", "m", "setm")]
+        public async Task ModifyRole(CommandContext ctx,
+                                    [Description("Role")] DiscordRole role = null,
+                                    [Description("True/False")] bool b = true)
+        {
+            if (role == null)
+                throw new ArgumentException("Unknown role.");
+
+            await ctx.Guild.UpdateRoleAsync(role, mentionable: b);
+            await ctx.RespondAsync("Successfully set " + role.Name + "'s mentionable var to: " + b);
         }
         #endregion
 
