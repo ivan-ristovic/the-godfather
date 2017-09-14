d625cf9df88dfe025776f703fa4c52045095ceda
Ivan Ristovic <ivo.ristovic@gmail.com>
2017-09-14 23:44:13 +0200

    Guild commands; Guild rename

---
+++ TheGodfather/Modules/CommandsGuild.cs
@@ -0,0 +1,35 @@
+﻿#region USING_DIRECTIVES
+using System;
+using System.Threading.Tasks;
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+#endregion
+
+namespace TheGodfatherBot
+{
+    [Group("guild", CanInvokeWithoutSubcommand = false)]
+    [Description("Miscellaneous guild control commands.")]
+    [Aliases("server", "g", "srv")]
+    [RequirePermissions(Permissions.ManageGuild)]
+    public class CommandsGuild
+    {
+        #region COMMAND_GUILD_RENAME
+        [Command("rename")]
+        [Description("Rename guild.")]
+        [Aliases("r", "name", "setname")]
+        public async Task RenameGuild(CommandContext ctx,
+                                     [RemainingText, Description("New name")] string name = null)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Missing new guild name.");
+
+            await ctx.Guild.ModifyAsync(name: name);
+            await ctx.RespondAsync("Guild successfully renamed.");
+        }
+        #endregion
+    }
+}
+
