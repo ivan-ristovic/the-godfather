83fb3562b928408bc8b5fad67d62216b2c592910
Ivan Ristovic <ivo.ristovic@gmail.com>
2017-09-14 21:35:03 +0200

    Messages deleteuser

--- TheGodfather/Modules/CommandsMessages.cs
+++ TheGodfather/Modules/CommandsMessages.cs
@@ -1,10 +1,13 @@
 ﻿#region USING_DIRECTIVES
 using System;
+using System.Linq;
+using System.Collections;
 using System.Threading.Tasks;
 
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 #endregion
 
 namespace TheGodfatherBot
@@ -17,15 +20,36 @@ namespace TheGodfatherBot
     {
         #region COMMAND_DELETE
         [Command("delete")]
-        [Aliases("prune", "del", "d")]
+        [Description("Deletes the specified ammount of most-recent messages from the channel.")]
+        [Aliases("-", "prune", "del", "d")]
         [RequirePermissions(Permissions.Administrator)]
-        public async Task Delete(CommandContext ctx, [Description("Ammount")] int n = 0)
+        public async Task Delete(CommandContext ctx, [Description("Ammount")] int n = 1)
         {
             if (n <= 0 || n > 10000)
                 throw new ArgumentOutOfRangeException("Invalid number of messages to delete (must be in range [1, 10000].");
 
             await ctx.Channel.GetMessagesAsync(n).ContinueWith(
                 async t => await ctx.Channel.DeleteMessagesAsync(t.Result)
+            );
+        }
+        #endregion
+
+        #region COMMAND_DELETE_FROM
+        [Command("deleteu")]
+        [Description("Deletes given ammount of most-recent messages from given user.")]
+        [Aliases("-user", "deluser", "du")]
+        [RequirePermissions(Permissions.Administrator)]
+        public async Task DeleteUserMessages(CommandContext ctx, 
+                                            [Description("User")] DiscordUser u = null,
+                                            [Description("Ammount")] int n = 1)
+        {
+            if (u == null)
+                throw new ArgumentException("User missing.");
+            if (n <= 0 || n > 10000)
+                throw new ArgumentOutOfRangeException("Invalid number of messages to delete (must be in range [1, 10000].");
+
+            await ctx.Channel.DeleteMessagesAsync(
+                ctx.Channel.GetMessagesAsync().Result.Where(m => m.Author.Id == u.Id).Take(n)
             );
         }
         #endregion
