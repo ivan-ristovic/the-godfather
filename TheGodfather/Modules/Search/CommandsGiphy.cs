7b568542564ca0c3002c50e6b0c603e50c899f70
Ivan Ristovic <ivo.ristovic@gmail.com>
2017-09-26 01:20:53 +0200

    Giphy base

---
+++ TheGodfather/Modules/Search/CommandsGiphy.cs
@@ -0,0 +1,32 @@
+﻿#region USING_DIRECTIVES
+using System;
+using System.Threading.Tasks;
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+#endregion
+
+namespace TheGodfatherBot.Modules.Search
+{
+    [Description("Giphy commands.")]
+    public class CommandsGiphy
+    {
+        #region STATIC_FIELDS
+
+        #endregion
+
+
+        #region COMMAND_GIPHY
+        [Command("giphy"), Description("Search GIPHY.")]
+        [Aliases("gif")]
+        public async Task Giphy(CommandContext ctx,
+                               [Description("Query.")] string q = null)
+        {
+            if (string.IsNullOrWhiteSpace(q))
+                throw new ArgumentException("Query missing!");
+            
+        }
+        #endregion
+    }
+}
