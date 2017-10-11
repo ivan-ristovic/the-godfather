9943c0551958e6df7c8ce396990454779f469f4f
Ivan Ristovic <ivo.ristovic@gmail.com>
2017-10-11 23:08:49 +0200

    [Jokes] Search improvement

--- TheGodfather/Commands/Search/CommandsJokes.cs
+++ TheGodfather/Commands/Search/CommandsJokes.cs
@@ -22,15 +22,10 @@ namespace TheGodfather.Commands.Search
     [Cooldown(2, 5, CooldownBucketType.User), Cooldown(4, 5, CooldownBucketType.Channel)]
     public class CommandsJokes
     {
-        #region PRIVATE_FIELDS
-        private const string _randjokeurl = "https://icanhazdadjoke.com/";
-        private const string _yomommaurl = "http://api.yomomma.info/";
-        #endregion
-
 
         public async Task ExecuteGroupAsync(CommandContext ctx)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(_randjokeurl);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://icanhazdadjoke.com/");
             request.AutomaticDecompression = DecompressionMethods.GZip;
             request.Accept = "text/plain";
 
@@ -43,15 +38,50 @@ namespace TheGodfather.Commands.Search
                         }
                     }
                 }
-                await ctx.RespondAsync(embed: new DiscordEmbedBuilder() { Description = data });
             } catch (WebException e) {
                 throw new CommandFailedException("Connection to remote site failed!", e);
             } catch (Exception e) {
                 throw new CommandFailedException("Exception occured!", e);
             }
 
+            await ctx.RespondAsync(embed: new DiscordEmbedBuilder() { Description = data });
         }
 
+
+        #region COMMAND_JOKE_SEARCH
+        [Command("search")]
+        [Description("Search for the joke containing the query.")]
+        [Aliases("s")]
+        public async Task SearchJoke(CommandContext ctx,
+                                    [RemainingText, Description("Query.")] string query = null)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+                throw new InvalidCommandUsageException("Query missing.");
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://icanhazdadjoke.com/search?term=" + query.Replace(' ', '+'));
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+            request.Accept = "text/plain";
+
+            string data = string.Empty;
+            try {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse()) {
+                    using (Stream stream = response.GetResponseStream()) {
+                        using (StreamReader reader = new StreamReader(stream)) {
+                            data = reader.ReadToEnd();
+                        }
+                    }
+                }
+                if (string.IsNullOrWhiteSpace(data))
+                    data = "No results...";
+            } catch (WebException e) {
+                throw new CommandFailedException("Connection to remote site failed!", e);
+            } catch (Exception e) {
+                throw new CommandFailedException("Exception occured!", e);
+            }
+
+            await ctx.RespondAsync(embed: new DiscordEmbedBuilder() { Description = string.Join("\n\n", data.Split('\n').Take(10)) });
+        }
+        #endregion
 
         #region COMMAND_JOKE_YOURMOM
         [Command("yourmom")]
@@ -61,7 +91,7 @@ namespace TheGodfather.Commands.Search
         {
             try {
                 var wc = new WebClient();
-                var data = wc.DownloadString(_yomommaurl);
+                var data = wc.DownloadString("http://api.yomomma.info/");
                 await ctx.RespondAsync(embed: new DiscordEmbedBuilder() {
                     Description = JObject.Parse(data)["joke"].ToString()
                 });
