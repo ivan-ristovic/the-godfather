04da95f448e70b6de2c746984e03f41bf4c55371
Ivan Ristovic <ivo.ristovic@gmail.com>
2017-10-12 12:23:10 +0200

    [Channel] Delete "this" group

--- TheGodfather/Commands/Administration/CommandsChannels.cs
+++ TheGodfather/Commands/Administration/CommandsChannels.cs
@@ -8,6 +8,7 @@ using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using DSharpPlus.Exceptions;
 #endregion
 
 namespace TheGodfather.Commands.Administration
@@ -75,7 +76,7 @@ namespace TheGodfather.Commands.Administration
                                        [Description("Channel.")] DiscordChannel c = null)
         {
             if (c == null)
-                throw new InvalidCommandUsageException("Channel missing.");
+                c = ctx.Channel;
 
             string name = c.Name;
             await c.DeleteAsync();
@@ -92,7 +93,7 @@ namespace TheGodfather.Commands.Administration
                                      [Description("Channel.")] DiscordChannel c = null)
         {
             if (c == null)
-                throw new InvalidCommandUsageException("Channel missing.");
+                c = ctx.Channel;
 
             var em = new DiscordEmbedBuilder() {
                 Title = "Details for channel: " + c.Name,
@@ -117,17 +118,22 @@ namespace TheGodfather.Commands.Administration
         [Aliases("r", "name", "setname")]
         [RequirePermissions(Permissions.ManageChannels)]
         public async Task RenameChannel(CommandContext ctx, 
-                                       [Description("Channel.")] DiscordChannel c = null,
-                                       [RemainingText, Description("New name.")] string name = null)
+                                       [Description("New name.")] string name = null,
+                                       [Description("Channel.")] DiscordChannel c = null)
         {
-            if (c == null)
-                throw new InvalidCommandUsageException("Channel missing.");
             if (string.IsNullOrWhiteSpace(name))
                 throw new InvalidCommandUsageException("Missing new channel name.");
             if (name.Contains(" "))
                 throw new InvalidCommandUsageException("Name cannot contain spaces.");
+            if (c == null)
+                c = ctx.Channel;
 
-            await c.ModifyAsync(name);
+            try {
+                await c.ModifyAsync(name);
+            } catch (BadRequestException e) {
+                throw new CommandFailedException("Error occured. Possibly the name entered contains invalid characters...", e);
+            }
+
             await ctx.RespondAsync("Channel successfully renamed.");
         }
         #endregion
@@ -138,71 +144,17 @@ namespace TheGodfather.Commands.Administration
         [Aliases("t", "topic")]
         [RequirePermissions(Permissions.ManageChannels)]
         public async Task SetChannelTopic(CommandContext ctx,
-                                         [Description("Channel.")] DiscordChannel c = null,
-                                         [RemainingText, Description("New topic.")] string topic = null)
+                                         [Description("New topic.")] string topic = null, 
+                                         [Description("Channel.")] DiscordChannel c = null)
         {
-            if (c == null)
-                throw new InvalidCommandUsageException("Channel missing.");
             if (string.IsNullOrWhiteSpace(topic))
                 throw new InvalidCommandUsageException("Missing topic.");
+            if (c == null)
+                c = ctx.Channel;
 
             await c.ModifyAsync(topic: topic);
             await ctx.RespondAsync("Channel topic successfully changed.");
         }
         #endregion
-
-
-        [Group("this", CanInvokeWithoutSubcommand = false)]
-        [Description("Control over current channel.")]
-        [Aliases("current", "cur", "curr")]
-        [Cooldown(3, 5, CooldownBucketType.Channel)]
-        public class CommandsThisChannel
-        {
-            #region COMMAND_CHANNEL_THIS_DELETE
-            [Command("delete")]
-            [Description("Delete this channel.")]
-            [Aliases("-", "del", "d", "remove")]
-            [RequirePermissions(Permissions.ManageChannels)]
-            public async Task DeleteChannel(CommandContext ctx)
-            {
-                await ctx.Channel.DeleteAsync();
-            }
-            #endregion
-
-            #region COMMAND_CHANNEL_THIS_RENAME
-            [Command("rename")]
-            [Description("Rename channel.")]
-            [Aliases("r", "name", "setname")]
-            [RequirePermissions(Permissions.ManageChannels)]
-            public async Task RenameChannel(CommandContext ctx,
-                                           [RemainingText, Description("New name.")] string name = null)
-            {
-                if (string.IsNullOrWhiteSpace(name))
-                    throw new InvalidCommandUsageException("Missing new channel name.");
-
-                if (name.Contains(" "))
-                    throw new ArgumentException("Name cannot contain spaces.");
-
-                await ctx.Channel.ModifyAsync(name);
-                await ctx.RespondAsync("Channel successfully renamed.");
-            }
-            #endregion
-
-            #region COMMAND_CHANNEL_THIS_SETTOPIC
-            [Command("settopic")]
-            [Description("Set channel topic.")]
-            [Aliases("t", "topic")]
-            [RequirePermissions(Permissions.ManageChannels)]
-            public async Task SetChannelTopic(CommandContext ctx,
-                                             [RemainingText, Description("New topic.")] string topic = null)
-            {
-                if (string.IsNullOrWhiteSpace(topic))
-                    throw new InvalidCommandUsageException("Missing topic.");
-
-                await ctx.Channel.ModifyAsync(topic: topic);
-                await ctx.RespondAsync("Channel topic successfully changed.");
-            }
-            #endregion
-        }
     }
 }
