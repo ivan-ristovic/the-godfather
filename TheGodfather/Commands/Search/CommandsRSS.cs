68ffbc62b37c60b2e7c7c19a63c102b2fef2f7d2
Ivan Ristovic <ivo.ristovic@gmail.com>
2017-10-18 16:11:58 +0200

    [RSS] Slight refactor

--- TheGodfather/Commands/Search/CommandsRSS.cs
+++ TheGodfather/Commands/Search/CommandsRSS.cs
@@ -1,8 +1,12 @@
 ﻿#region USING_DIRECTIVES
 using System;
 using System.Xml;
+using System.Linq;
+using System.Collections.Generic;
 using System.ServiceModel.Syndication;
 using System.Threading.Tasks;
+
+using TheGodfather.Exceptions;
 
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
@@ -20,17 +24,18 @@ namespace TheGodfather.Commands.Search
         public async Task ExecuteGroupAsync(CommandContext ctx, 
                                             [RemainingText, Description("URL")] string url = null)
         {
-            if (string.IsNullOrWhiteSpace(url))
+            if (string.IsNullOrWhiteSpace(url)) {
                 await WMRSS(ctx);
-            else
-                await RSSFeedRead(ctx, url);
+            } else {
+                await SendFeedResults(ctx, GetFeedResults(url));
+            }
         }
 
         #region COMMAND_WM
         [Command("wm"), Description("Get newest topics from WM forum.")]
         public async Task WMRSS(CommandContext ctx)
         {
-            await RSSFeedRead(ctx, "http://worldmafia.net/forum/forums/-/index.rss");
+            await SendFeedResults(ctx, GetFeedResults("http://worldmafia.net/forum/forums/-/index.rss"));
         }
         #endregion
 
@@ -38,34 +43,40 @@ namespace TheGodfather.Commands.Search
         [Command("news"), Description("Get newest world news.")]
         public async Task NewsRSS(CommandContext ctx)
         {
-            await RSSFeedRead(ctx, "https://news.google.com/news/rss/headlines/section/topic/WORLD?ned=us&hl=en");
+            await SendFeedResults(ctx, GetFeedResults("https://news.google.com/news/rss/headlines/section/topic/WORLD?ned=us&hl=en"));
         }
         #endregion
 
         #region HELPER_FUNCTIONS
-        private async Task RSSFeedRead(CommandContext ctx, string url)
+        private IEnumerable<SyndicationItem> GetFeedResults(string url)
         {
             SyndicationFeed feed = null;
+            XmlReader reader = null;
             try {
-                XmlReader reader = XmlReader.Create(url);
+                reader = XmlReader.Create(url);
                 feed = SyndicationFeed.Load(reader);
                 reader.Close();
             } catch (Exception) {
-                await ctx.RespondAsync("Error getting RSS feed from " + url);
-                return;
+                return null;
+            } finally {
+                reader?.Close();
             }
+
+            return feed.Items;
+        }
+
+        private async Task SendFeedResults(CommandContext ctx, IEnumerable<SyndicationItem> results)
+        {
+            if (results == null)
+                throw new CommandFailedException("Error getting RSS feed.");
 
             var embed = new DiscordEmbedBuilder() {
                 Title = "Topics active recently",
                 Color = DiscordColor.Green
             };
 
-            int count = 5;
-            foreach (SyndicationItem item in feed.Items) {
-                if (count-- == 0)
-                    break;
-                embed.AddField(item.Title.Text, item.Links[0].Uri.ToString());
-            }
+            foreach (var res in results.Take(5))
+                embed.AddField(res.Title.Text, res.Links[0].Uri.ToString());
 
             await ctx.RespondAsync(embed: embed);
         }
