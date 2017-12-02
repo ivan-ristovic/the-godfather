b37939880d74ba9f7fdc641763e28991ecdff1f1
Ivan Ristovic <ivo.ristovic@gmail.com>
2017-12-02 21:40:17 +0100

    [Games] Hangman enhancement

--- TheGodfather/Commands/Games/Common/Hangman.cs
+++ TheGodfather/Commands/Games/Common/Hangman.cs
@@ -20,7 +20,12 @@ namespace TheGodfather.Commands.Games
     public class Hangman
     {
         #region STATIC_FIELDS
+        public static bool GameExistsInChannel(ulong cid) => _channels.Contains(cid);
         private static ConcurrentHashSet<ulong> _channels = new ConcurrentHashSet<ulong>();
+        #endregion
+
+        #region PUBLIC_FIELDS
+        public DiscordUser Winner { get; private set; }
         #endregion
 
         #region PRIVATE_FIELDS
@@ -43,9 +48,7 @@ namespace TheGodfather.Commands.Games
             _word = word.ToLower();
             _hidden = word.Select(c => (c == ' ') ? '.' : '?').ToArray();
         }
-
-
-        public static bool GameExistsInChannel(ulong cid) => _channels.Contains(cid);
+        
 
         public async Task PlayAsync()
         {
@@ -57,7 +60,7 @@ namespace TheGodfather.Commands.Games
 
             await UpdateHangmanAsync()
                 .ConfigureAwait(false);
-            while (!_gameOver && _lives > 0 && Array.IndexOf(_hidden, '?') != -1)
+            while (!_gameOver && _lives > 0)
                 await AdvanceAsync().ConfigureAwait(false);
 
             await channel.SendMessageAsync("Game over! The word was: " + Formatter.Bold(_word))
@@ -70,11 +73,11 @@ namespace TheGodfather.Commands.Games
         {
             var interactivity = _client.GetInteractivityModule();
             var m = await interactivity.WaitForMessageAsync(
-                    xm => xm.Channel.Id == _cid && 
+                    xm => (xm.Channel.Id == _cid && 
                           !xm.Author.IsBot && 
                           xm.Content.Length == 1 && 
                           Char.IsLetterOrDigit(xm.Content[0]) &&
-                          !_badguesses.Contains(xm.Content[0])
+                          !_badguesses.Contains(xm.Content[0])) || xm.Content.ToLower() == _word
                     , TimeSpan.FromMinutes(1)
                 ).ConfigureAwait(false);
             if (m == null) {
@@ -82,11 +85,20 @@ namespace TheGodfather.Commands.Games
                 return;
             }
 
+            if (m.Message.Content.ToLower() == _word) {
+                Winner = m.User;
+                _gameOver = true;
+            }
+
             char guess_char = Char.ToLower(m.Message.Content[0]);
             if (_word.IndexOf(guess_char) != -1) {
                 for (int i = 0; i < _word.Length; i++)
                     if (_word[i] == guess_char)
                         _hidden[i] = Char.ToUpper(_word[i]);
+                if (Array.IndexOf(_hidden, '?') == -1) {
+                    Winner = m.User;
+                    _gameOver = true;
+                }
             } else {
                 _lives--;
                 _badguesses.Add(guess_char);
