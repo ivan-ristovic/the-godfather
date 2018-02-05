62af3ceaae65d12c25ca09e168a76ffedacc1792
ivan-ristovic <ivo.ristovic@gmail.com>
2018-02-05 14:23:44 +0100

    [Gamble] Slot refactor

--- TheGodfather/Modules/Gambling/GambleModule.cs
+++ TheGodfather/Modules/Gambling/GambleModule.cs
@@ -1,16 +1,10 @@
 ﻿#region USING_DIRECTIVES
 using System;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
 
 using TheGodfather.Attributes;
 using TheGodfather.Services;
-using TheGodfather.Exceptions;
-
-using DSharpPlus;
-using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
 #endregion
 
 namespace TheGodfather.Modules.Gambling
@@ -22,108 +16,6 @@ namespace TheGodfather.Modules.Gambling
     [ListeningCheck]
     public partial class GambleModule : GodfatherBaseModule
     {
-
         public GambleModule(DatabaseService db) : base(db: db) { }
-
-
-        #region COMMAND_SLOT
-        [Command("slot")]
-        [Description("Roll a slot machine.")]
-        [Aliases("slotmachine")]
-        public async Task SlotMachine(CommandContext ctx,
-                                     [Description("Bid.")] int bid = 5)
-        {
-            if (bid < 5)
-                throw new CommandFailedException("5 is the minimum bid!", new ArgumentOutOfRangeException());
-
-            if (!await ctx.Services.GetService<DatabaseService>().RetrieveCreditsAsync(ctx.User.Id, bid).ConfigureAwait(false))
-                throw new CommandFailedException("You do not have enough credits in WM bank!");
-
-            var slot_res = RollSlot(ctx);
-            int won = EvaluateSlotResult(slot_res, bid);
-
-            var em = new DiscordEmbedBuilder() {
-                Title = "TOTALLY NOT RIGGED SLOT MACHINE",
-                Description = MakeStringFromResult(slot_res),
-                Color = DiscordColor.Yellow
-            };
-            em.AddField("Result", $"You won {Formatter.Bold(won.ToString())} credits!");
-
-            await ctx.RespondAsync(embed: em.Build())
-                .ConfigureAwait(false);
-
-            if (won > 0)
-                await ctx.Services.GetService<DatabaseService>().IncreaseBalanceForUserAsync(ctx.User.Id, won)
-                    .ConfigureAwait(false);
-        }
-        #endregion
-
-        #region HELPER_FUNCTIONS
-        private DiscordEmoji[,] RollSlot(CommandContext ctx)
-        {
-            DiscordEmoji[] emoji = {
-                DiscordEmoji.FromName(ctx.Client, ":peach:"),
-                DiscordEmoji.FromName(ctx.Client, ":moneybag:"),
-                DiscordEmoji.FromName(ctx.Client, ":gift:"),
-                DiscordEmoji.FromName(ctx.Client, ":large_blue_diamond:"),
-                DiscordEmoji.FromName(ctx.Client, ":seven:"),
-                DiscordEmoji.FromName(ctx.Client, ":cherries:")
-            };
-
-            var rnd = new Random();
-            DiscordEmoji[,] result = new DiscordEmoji[3, 3];
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
-                    result[i, j] = emoji[rnd.Next(0, emoji.Length)];
-
-            return result;
-        }
-
-        private string MakeStringFromResult(DiscordEmoji[,] res)
-        {
-            string s = "";
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++)
-                    s += res[i, j].ToString();
-                s += '\n';
-            }
-            return s;
-        }
-
-        private int EvaluateSlotResult(DiscordEmoji[,] res, int bid)
-        {
-            int pts = bid;
-
-            // Rows
-            for (int i = 0; i < 3; i++) {
-                if (res[i, 0] == res[i, 1] && res[i, 1] == res[i, 2]) {
-                    if (res[i, 0].ToString() == ":large_blue_diamond:")
-                        pts *= 50;
-                    else if (res[i, 0].ToString() == ":moneybag:")
-                        pts *= 25;
-                    else if (res[i, 0].ToString() == ":seven:")
-                        pts *= 10;
-                    else
-                        pts *= 5;
-                }
-            }
-
-            // Columns
-            for (int i = 0; i < 3; i++) {
-                if (res[0, i] == res[1, i] && res[1, i] == res[2, i]) {
-                    if (res[0, i].ToString() == ":large_blue_diamond:")
-                        pts *= 50;
-                    else if (res[0, i].ToString() == ":moneybag:")
-                        pts *= 25;
-                    else if (res[0, i].ToString() == ":seven:")
-                        pts *= 10;
-                    else
-                        pts *= 5;
-                }
-            }
-
-            return pts == bid ? 0 : pts;
-        }
-        #endregion
     }
 }
