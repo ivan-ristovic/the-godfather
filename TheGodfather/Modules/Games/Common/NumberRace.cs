4c0d5346040d5445e1a020f753b66dc1cfb23f67
ivan-ristovic <ivo.ristovic@gmail.com>
2018-02-14 21:38:54 +0100

    [Nunchi] Refactor; Rename

---
+++ TheGodfather/Modules/Games/Common/NumberRace.cs
@@ -0,0 +1,73 @@
+﻿#region USING_DIRECTIVES
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TheGodfather.Extensions.Collections;
+using TheGodfather.Modules.Games.Common;
+
+using DSharpPlus.Interactivity;
+using DSharpPlus.Entities;
+#endregion
+
+namespace TheGodfather.Modules.Games
+{
+    public class NumberRace : Game
+    {
+        private ConcurrentHashSet<DiscordUser> _participants = new ConcurrentHashSet<DiscordUser>();
+        public int ParticipantCount => _participants.Count();
+        public bool GameStarted { get; private set; }
+
+
+        public NumberRace(InteractivityExtension interactivity, DiscordChannel channel)
+            : base(interactivity, channel)
+        {
+            GameStarted = false;
+        }
+
+
+        public override async Task RunAsync()
+        {
+            GameStarted = true;
+
+            int num = new Random().Next(1000);
+            await _channel.SendMessageAsync(num.ToString())
+                .ConfigureAwait(false);
+            
+            while (ParticipantCount > 1) {
+                int guess = 0;
+                var mctx = await _interactivity.WaitForMessageAsync(
+                    xm => {
+                        if (xm.Channel.Id != _channel.Id || xm.Author.IsBot) return false;
+                        if (!_participants.Contains(xm.Author)) return false;
+                        return int.TryParse(xm.Content, out guess);
+                    },
+                    TimeSpan.FromSeconds(20)
+                ).ConfigureAwait(false);
+
+                if (mctx == null) {
+                    NoReply = true;
+                    return;
+                } else if (guess == num + 1) {
+                    num++;
+                    Winner = mctx.User;
+                } else {
+                    await _channel.SendMessageAsync(mctx.User.Mention + " lost!")
+                        .ConfigureAwait(false);
+                    if (Winner != null && Winner.Id == mctx.User.Id)
+                        Winner = null;
+                    _participants.RemoveWhere(u => mctx.User.Id == u.Id);
+                }
+            }
+
+            Winner = _participants.First();
+        }
+
+        public bool AddParticipant(DiscordUser user)
+        {
+            if (_participants.Any(u => user.Id == u.Id))
+                return false;
+            return _participants.Add(user);
+        }
+    }
+}
---
+++ TheGodfather/Modules/Games/GamesModule.NumberRace.cs
@@ -0,0 +1,129 @@
+﻿#region USING_DIRECTIVES
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+using TheGodfather.Attributes;
+using TheGodfather.Services;
+using TheGodfather.Exceptions;
+using TheGodfather.Modules.Games.Common;
+
+using DSharpPlus;
+using DSharpPlus.Interactivity;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+#endregion
+
+namespace TheGodfather.Modules.Games
+{
+    public partial class GamesModule
+    {
+        [Group("numberrace")]
+        [Description("Number racing game commands.")]
+        [Aliases("n", "nunchi", "numbers", "numbersrace")]
+        [UsageExample("!game numberrace")]
+        [Cooldown(2, 5, CooldownBucketType.User), Cooldown(3, 5, CooldownBucketType.Channel)]
+        [ListeningCheck]
+        public class NumberRaceModule : GodfatherBaseModule
+        {
+
+            public NumberRaceModule(DatabaseService db) : base(db: db) { }
+
+
+            [GroupCommand]
+            public async Task ExecuteGroupAsync(CommandContext ctx)
+            {
+                if (Game.RunningInChannel(ctx.Channel.Id)) {
+                    if (Game.GetRunningGameInChannel(ctx.Channel.Id) is NumberRace)
+                        await JoinGameAsync(ctx).ConfigureAwait(false);
+                    else
+                        throw new CommandFailedException("Another game is already running in the current channel.");
+                    return;
+                }
+
+                var game = new NumberRace(ctx.Client.GetInteractivity(), ctx.Channel);
+                Game.RegisterGameInChannel(game, ctx.Channel.Id);
+                try {
+                    await ReplyWithEmbedAsync(ctx, $"The race will start in 30s or when there are 10 participants. Type {Formatter.InlineCode("!game numberrace")} to join the race.", ":clock1:")
+                        .ConfigureAwait(false);
+                    await JoinGameAsync(ctx)
+                        .ConfigureAwait(false);
+                    await Task.Delay(TimeSpan.FromSeconds(30))
+                        .ConfigureAwait(false);
+
+                    if (game.ParticipantCount > 1) {
+                        await game.RunAsync()
+                            .ConfigureAwait(false);
+
+                        if (game.NoReply) {
+                            if (game.Winner != null) {
+                                await ReplyWithEmbedAsync(ctx, $"{game.Winner.Mention} won due to no replies from other users!", ":trophy:")
+                                    .ConfigureAwait(false);
+                            } else {
+                                await ReplyWithEmbedAsync(ctx, "No replies, aborting Number Race...", ":alarm_clock:")
+                                        .ConfigureAwait(false);
+                            }
+                        } else {
+                            await ReplyWithEmbedAsync(ctx, "Winner: " + game.Winner.Mention, ":trophy:")
+                                .ConfigureAwait(false);
+                        }
+
+                        if (game.Winner != null)
+                            await DatabaseService.UpdateUserStatsAsync(game.Winner.Id, "nunchis_won")
+                                .ConfigureAwait(false);
+                    } else {
+                        await ReplyWithEmbedAsync(ctx, "Not enough users joined the race.", ":alarm_clock:")
+                            .ConfigureAwait(false);
+                    }
+                } finally {
+                    Game.UnregisterGameInChannel(ctx.Channel.Id);
+                }
+            }
+
+
+            #region COMMAND_NUMBERRACE_JOIN
+            [Command("join")]
+            [Description("Join an existing number race game.")]
+            [Aliases("+", "compete", "j", "enter")]
+            [UsageExample("!game numberrace join")]
+            public async Task JoinGameAsync(CommandContext ctx)
+            {
+                var game = Game.GetRunningGameInChannel(ctx.Channel.Id) as NumberRace;
+                if (game == null)
+                    throw new CommandFailedException("There is no number race game running in this channel.");
+
+                if (game.GameStarted)
+                    throw new CommandFailedException("Race has already started, you can't join it.");
+
+                if (game.ParticipantCount >= 10)
+                    throw new CommandFailedException("Race slots are full (max 10 participants), kthxbye.");
+
+                if (!game.AddParticipant(ctx.User))
+                    throw new CommandFailedException("You are already participating in the race!");
+
+                await ReplyWithEmbedAsync(ctx, $"{ctx.User.Mention} joined the game.", ":bicyclist:")
+                    .ConfigureAwait(false);
+            }
+            #endregion
+
+            #region COMMAND_NUMBERRACE_RULES
+            [Command("rules")]
+            [Description("Explain the number race rules.")]
+            [Aliases("help", "h", "ruling", "rule")]
+            [UsageExample("!game numberrace rules")]
+            public async Task RulesAsync(CommandContext ctx)
+            {
+                await ReplyWithEmbedAsync(
+                    ctx,
+                    "I will start by typing a number. Users have to count up by 1 from that number. " +
+                    "If someone makes a mistake (types an incorrent number, or repeats the same number) " +
+                    "they are out of the game. If nobody posts a number 20s after the last number was posted, " +
+                    "then the user that posted that number wins the game. The game ends when only one user remains."
+                    , ":information_source:"
+                ).ConfigureAwait(false);
+            }
+            #endregion
+        }
+    }
+}
