ebf749a1fd8c20e8cf70db39b1523118841c7efa
ivan-ristovic <ivo.ristovic@gmail.com>
2018-02-21 14:50:54 +0100

    [Help] Enhanced help with examples and perms

--- TheGodfather/Extensions/CustomHelpFormatter.cs
+++ TheGodfather/Extensions/CustomHelpFormatter.cs
@@ -1,11 +1,13 @@
 ﻿#region USING_DIRECTIVES
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using TheGodfather.Attributes;
+
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.CommandsNext.Converters;
 using DSharpPlus.CommandsNext.Entities;
 using DSharpPlus.Entities;
@@ -42,40 +44,76 @@ namespace TheGodfather.Extensions
 
         public override BaseHelpFormatter WithCommand(Command command)
         {
-            _name = command.QualifiedName;
+            _name = command is CommandGroup ? $"Group: {command.QualifiedName}" : command.QualifiedName;
             _desc = command.Description;
 
             if (command.Aliases?.Any() == true)
                 _emb.AddField("Aliases", string.Join(", ", command.Aliases.Select(a => Formatter.InlineCode(a))));
 
+            var allchecks = command.ExecutionChecks.Union(command.Parent?.ExecutionChecks ?? Enumerable.Empty<CheckBaseAttribute>());
+            var permissions = allchecks.Where(chk => chk is RequirePermissionsAttribute)
+                                       .Select(chk => chk as RequirePermissionsAttribute)
+                                       .Select(chk => chk.Permissions.ToPermissionString());
+            var userpermissions = allchecks.Where(chk => chk is RequireUserPermissionsAttribute)
+                                           .Select(chk => chk as RequireUserPermissionsAttribute)
+                                           .Select(chk => chk.Permissions.ToPermissionString());
+            var botpermissions = allchecks.Where(chk => chk is RequireBotPermissionsAttribute)
+                                          .Select(chk => chk as RequireBotPermissionsAttribute)
+                                          .Select(chk => chk.Permissions.ToPermissionString());
+
+            var pb = new StringBuilder();
+            if (allchecks.Any(chk => chk is RequireOwnerAttribute))
+                pb.AppendLine(Formatter.Bold("Owner-only."));
+            if (permissions.Any()) 
+                pb.AppendLine(Formatter.InlineCode(string.Join(", ", permissions)));
+            if (userpermissions.Any()) 
+                pb.Append(Formatter.Bold("User permissions: ")).AppendLine(Formatter.InlineCode(string.Join(", ", userpermissions)));
+            if (botpermissions.Any()) 
+                pb.Append(Formatter.Bold("Bot permissions: ")).AppendLine(Formatter.InlineCode(string.Join(", ", botpermissions)));
+
+            string perms = pb.ToString();
+            _emb.AddField("Required permissions", string.IsNullOrWhiteSpace(perms) ? "None" : perms);
+
             if (command.Overloads?.Any() == true) {
-                foreach (var overload in command.Overloads.OrderByDescending(x => x.Priority)) {
-                    var sb = new StringBuilder();
+                foreach (var overload in command.Overloads.OrderByDescending(o => o.Priority)) {
+                    var ab = new StringBuilder();
 
                     foreach (var arg in overload.Arguments) {
                         if (arg.IsOptional)
-                            sb.Append("(optional) ");
+                            ab.Append("(optional) ");
 
                         string typestr = $"[{CommandsNext.GetUserFriendlyTypeName(arg.Type)}";
                         if (arg.IsCatchAll)
                             typestr += "...";
                         typestr += "]";
 
-                        sb.Append(Formatter.InlineCode(typestr));
-                        sb.Append(" ");
+                        ab.Append(Formatter.InlineCode(typestr));
+                        ab.Append(" ");
 
-                        sb.Append(string.IsNullOrWhiteSpace(arg.Description) ? "No description provided." : Formatter.Bold(arg.Description));
+                        ab.Append(string.IsNullOrWhiteSpace(arg.Description) ? "No description provided." : Formatter.Bold(arg.Description));
 
                         if (arg.IsOptional)
-                            sb.Append(" (def: ").Append(Formatter.InlineCode(arg.DefaultValue != null ? arg.DefaultValue.ToString() : "None")).Append(")");
+                            ab.Append(" (def: ").Append(Formatter.InlineCode(arg.DefaultValue != null ? arg.DefaultValue.ToString() : "None")).Append(")");
 
-                        sb.AppendLine();
+                        ab.AppendLine();
                     }
 
-                    string args = sb.ToString();
+                    string args = ab.ToString();
                     _emb.AddField($"{(command.Overloads.Count > 1 ? $"Overload #{overload.Priority}" : "Arguments")}" , string.IsNullOrWhiteSpace(args) ? "None": args);
                 }
             }
+
+            var examples = command.CustomAttributes.Where(chk => chk is UsageExampleAttribute)
+                                                   .Select(chk => chk as UsageExampleAttribute);
+            var eb = new StringBuilder();
+            if (examples.Any()) {
+                eb.AppendLine("```");
+                foreach (var example in examples)
+                    eb.AppendLine(example.Example);
+                eb.AppendLine("```");
+            }
+            string examplestr = eb.ToString();
+            _emb.AddField("Examples", string.IsNullOrWhiteSpace(examplestr) ? "None" : examplestr);
 
             return this;
         }
