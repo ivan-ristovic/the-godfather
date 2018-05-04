6f86fe2b2f2417591341a4c78c8ec389bee45d75
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-05-05 00:33:46 +0200

    [Services] Quote service

---
+++ TheGodfather/Services/Common/Quotes.cs
@@ -0,0 +1,65 @@
+﻿#region USING_DIRECTIVES
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+using DSharpPlus;
+using DSharpPlus.Entities;
+#endregion
+
+namespace TheGodfather.Services.Common
+{
+    public class QuoteApiResponse
+    {
+        [JsonProperty("success")]
+        public QuoteApiSuccess Success { get; set; }
+
+        [JsonProperty("contents")]
+        public QuoteApiContent Contents { get; set; }
+    }
+
+    public class QuoteApiSuccess
+    {
+        [JsonProperty("total")]
+        public int Count { get; set; }
+    }
+
+    public class QuoteApiContent
+    {
+        [JsonProperty("quotes")]
+        public List<Quote> Quotes { get; set; }
+    }
+
+    public class Quote
+    {
+        [JsonProperty("quote")]
+        public string Content { get; set; }
+
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("permalink")]
+        public string Permalink { get; set; }
+
+        [JsonProperty("background")]
+        public string BackgroundImageUrl { get; set; }
+
+
+        public DiscordEmbed Embed(string title = null)
+        {
+            var emb = new DiscordEmbedBuilder() {
+                Title = title ?? "Quote",
+                Description = Formatter.Italic($"\"{Content}\""),
+                Color = DiscordColor.SpringGreen,
+                ThumbnailUrl = BackgroundImageUrl,
+                Url = Permalink
+            };
+            emb.AddField("Author", Author);
+            emb.WithFooter("Powered by theysaidso.com");
+
+            return emb.Build();
+        }
+    }
+}
---
+++ TheGodfather/Services/QuoteService.cs
@@ -0,0 +1,41 @@
+﻿#region USING_DIRECTIVES
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using TheGodfather.Services.Common;
+
+using DSharpPlus;
+using System.Net.Http;
+using System.Net.Http.Headers;
+#endregion;
+
+namespace TheGodfather.Services
+{
+    public class QuoteService : TheGodfatherHttpService
+    {
+        public static async Task<Quote> GetQuoteOfTheDayAsync()
+        {
+            try {
+                var request = new HttpRequestMessage();
+                request.RequestUri = new Uri("https://quotes.rest/qod");
+                request.Headers.Accept.Clear();
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await _http.SendAsync(request)
+                    .ConfigureAwait(false);
+                if (!response.IsSuccessStatusCode)
+                    return null;
+                var json = await response.Content.ReadAsStringAsync()
+                    .ConfigureAwait(false);
+
+                var quoteResponse = JsonConvert.DeserializeObject<QuoteApiResponse>(json);
+                return quoteResponse.Contents.Quotes.FirstOrDefault();
+            } catch (Exception e) {
+                TheGodfather.LogHandle.LogException(LogLevel.Debug, e);
+                return null;
+            }
+        }
+    }
+}
