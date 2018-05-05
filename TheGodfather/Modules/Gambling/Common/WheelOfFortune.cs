593e55465351720b0f6872db44378f05e09d47e2
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-05-05 14:20:46 +0200

    [Gambling] Simplified WheelOfFortune to counter ratelimit hits

--- TheGodfather/Modules/Gambling/Common/WheelOfFortune.cs
+++ TheGodfather/Modules/Gambling/Common/WheelOfFortune.cs
@@ -1,5 +1,4 @@
 ﻿#region USING_DIRECTIVES
-using System;
 using System.Collections.Immutable;
 using System.Threading.Tasks;
 
@@ -22,14 +21,12 @@ namespace TheGodfather.Modules.Gambling.Common
         private static readonly ImmutableArray<float> Multipliers = new float[] {
             2.4f, 0.3f, 1.7f, 0.5f, 1.2f, 0.1f, 0.2f, 1.5f
         }.ToImmutableArray();
-
-        public int Duration { get; }
-        public float Multiplier => Multipliers[Duration % Multipliers.Length];
-        public int WonAmount { get; private set; }
-        public bool Rotating = false;
+        
+        public int WonAmount => (int)(_bid * Multipliers[_index]);
 
         private DiscordUser _user;
-        private int _bid;
+        private int _bid = 0;
+        private int _index = 0;
 
 
         public WheelOfFortune(InteractivityExtension interactivity, DiscordChannel channel, DiscordUser user, int bid)
@@ -37,54 +34,29 @@ namespace TheGodfather.Modules.Gambling.Common
         {
             _user = user;
             _bid = bid;
-            Duration = GFRandom.Generator.Next(3, 12);
-            WonAmount = (int)(_bid * Multiplier);
+            _index = GFRandom.Generator.Next(Multipliers.Length);
         }
 
 
         public override async Task RunAsync()
         {
-            Rotating = true;
-
-            var msg = await _channel.SendMessageAsync($"Rolling Wheel of Fortune for {_user.Mention}!")
-                .ConfigureAwait(false);
-
-            for (int i = 0; i < Duration; i++) {
-                await PrintGameAsync(msg, i)
-                    .ConfigureAwait(false);
-                await Task.Delay(TimeSpan.FromSeconds(1))
-                    .ConfigureAwait(false);
-            }
-
-            Rotating = false;
-
-            await Task.Delay(TimeSpan.FromSeconds(1))
-                .ConfigureAwait(false);
-            await PrintGameAsync(msg, Duration)
-                .ConfigureAwait(false);
-        }
-
-
-        private async Task PrintGameAsync(DiscordMessage msg, int rotation)
-        {
             var emb = new DiscordEmbedBuilder() {
-                Title = $"{StaticDiscordEmoji.MoneyBag} WHEEL OF FORTUNE {StaticDiscordEmoji.MoneyBag}",
+                Title = $"{StaticDiscordEmoji.MoneyBag} WM WHEEL OF FORTUNE! {StaticDiscordEmoji.MoneyBag}",
                 Color = DiscordColor.Yellow
             };
 
             string description = $@"
 ‣‣‣‣‣‣‣‣‣‣『{Multipliers[0]}』‣‣‣‣‣‣‣‣‣‣
 ‣‣‣『{Multipliers[7]}』         『{Multipliers[1]}』‣‣‣
-『{Multipliers[6]}』    {_emojis[rotation % Multipliers.Length]}       『{Multipliers[2]}』
+『{Multipliers[6]}』    {_emojis[_index]}       『{Multipliers[2]}』
 ‣‣‣『{Multipliers[5]}』         『{Multipliers[3]}』‣‣‣
 ‣‣‣‣‣‣‣‣‣‣『{Multipliers[4]}』‣‣‣‣‣‣‣‣‣‣";
-            
+
             emb.WithDescription(description);
 
-            if (!Rotating)
-                emb.AddField("Result", $"{_user.Mention} won {Formatter.Bold(WonAmount.ToString())} credits!");
+            emb.AddField("Result", $"{_user.Mention} won {Formatter.Bold(WonAmount.ToString())} credits!");
 
-            await msg.ModifyAsync(embed: emb.Build())
+            await _channel.SendMessageAsync(embed: emb.Build())
                 .ConfigureAwait(false);
         }
     }
