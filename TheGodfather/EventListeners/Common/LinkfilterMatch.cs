620d2821db3c359a6c9207c6e32560d5155296c0
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-05-27 11:12:25 +0200

    [Linkfilter] Helper classes

---
+++ TheGodfather/EventListeners/Common/LinkfilterMatch.cs
@@ -0,0 +1,41 @@
+﻿#region USING_DIRECTIVES
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using DSharpPlus.Entities;
+#endregion
+
+namespace TheGodfather.EventListeners.Common
+{
+    public class LinkfilterMatch
+    {
+        public bool Success { get; }
+        public string Matched { get; }
+
+
+        public LinkfilterMatch(Match match)
+        {
+            Success = match.Success;
+            Matched = match.Groups[0].Value;
+        }
+    }
+
+    public class LinkfilterMatcher : Regex
+    {
+        public LinkfilterMatcher(params string[] items) 
+            : base($@"({string.Join("|", items.Select(Escape))})", RegexOptions.IgnoreCase)
+        {
+
+        }
+
+        public LinkfilterMatcher(IEnumerable<string> items) 
+            : base($@"({string.Join("|", items.Select(Escape))})", RegexOptions.IgnoreCase)
+        {
+
+        }
+
+        public LinkfilterMatch Check(DiscordMessage message)
+            => new LinkfilterMatch(Match(message.Content));
+    }
+}
