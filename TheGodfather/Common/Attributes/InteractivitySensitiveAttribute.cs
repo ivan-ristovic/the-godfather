00ab45fd0977aeeba706ed7dab69742026bc8324
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-06-15 00:14:05 +0200

    [Attributes] Add InteractivitySensitiveAttribute

---
+++ TheGodfather/Common/Attributes/InteractivitySensitiveAttribute.cs
@@ -0,0 +1,24 @@
+﻿#region USING_DIRECTIVES
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+#endregion
+
+namespace TheGodfather.Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    internal class InteractivitySensitiveAttribute : CheckBaseAttribute
+    {
+        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        {
+            var shared = ctx.Services.GetService<SharedData>();
+            if (shared.AwaitingUsersInteractively.ContainsKey(ctx.Channel.Id) && shared.AwaitingUsersInteractively[ctx.Channel.Id].Contains(ctx.User.Id))
+                return Task.FromResult(false);
+            else
+                return Task.FromResult(true);
+        }
+    }
+}
