ca58c5d525b2c5f737e2f89336c052090e5bc929
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-07-09 22:16:21 +0200

    [Tests] GiphyService

--- TheGodfatherTests/Services/GiphyServiceTests.cs
+++ TheGodfatherTests/Services/GiphyServiceTests.cs
@@ -1,27 +1,25 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using TheGodfather.Common;
-using TheGodfather.Services;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using ImageData = GiphyDotNet.Model.GiphyImage.Data;
+using RandomImageData = GiphyDotNet.Model.GiphyRandomImage.Data;
 
 namespace TheGodfather.Services.Tests
 {
     [TestClass()]
     public class GiphyServiceTests
     {
-        private static GiphyService _service { get; set; }
+        private static GiphyService _service;
 
 
         [ClassInitialize]
         public static async Task Init(TestContext ctx)
         {
-            string json;
             try {
+                string json;
                 using (var sr = new StreamReader("Resources/config.json"))
                     json = await sr.ReadToEndAsync();
                 var cfg = JsonConvert.DeserializeObject<BotConfig>(json);
@@ -35,21 +33,91 @@ namespace TheGodfather.Services.Tests
         [TestMethod()]
         public async Task SearchAsyncTest()
         {
-            
+            ImageData[] results;
+
+            results = await _service.SearchAsync("test");
+            Assert.IsNotNull(results);
+            Assert.AreEqual(results.Length, 1);
+            Assert.IsNotNull(results[0]);
+            Assert.IsNotNull(results[0].Url);
+
+            results = await _service.SearchAsync("test", 5);
+            Assert.IsNotNull(results);
+            Assert.AreEqual(results.Length, 5);
+            foreach (ImageData result in results) {
+                Assert.IsNotNull(result);
+                Assert.IsNotNull(result.Url);
+            }
+
+            results = await _service.SearchAsync("test", 15);
+            Assert.IsNotNull(results);
+            Assert.AreEqual(results.Length, 15);
+
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.SearchAsync("test", 0);
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.SearchAsync("test", -5);
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.SearchAsync("test", 100);
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.SearchAsync("test", 21);
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.SearchAsync(null);
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.SearchAsync("");
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.SearchAsync("  ");
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.SearchAsync("\n");
+            });
         }
 
         [TestMethod()]
         public async Task GetRandomGifAsyncTest()
         {
-            var data = await _service.GetRandomGifAsync();
+            RandomImageData data = await _service.GetRandomGifAsync();
             Assert.IsNotNull(data);
             Assert.IsNotNull(data.Url);
         }
 
         [TestMethod()]
-        public void GetTrendingGifsAsyncTest()
+        public async Task GetTrendingGifsAsyncTest()
         {
+            ImageData[] results;
 
+            results = await _service.GetTrendingGifsAsync();
+            Assert.IsNotNull(results);
+            Assert.AreEqual(results.Length, 1);
+            Assert.IsNotNull(results[0]);
+            Assert.IsNotNull(results[0].Url);
+
+            results = await _service.GetTrendingGifsAsync(5);
+            Assert.IsNotNull(results);
+            Assert.AreEqual(results.Length, 5);
+            foreach (ImageData result in results) {
+                Assert.IsNotNull(result);
+                Assert.IsNotNull(result.Url);
+            }
+
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.GetTrendingGifsAsync(0);
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.GetTrendingGifsAsync(-1);
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.GetTrendingGifsAsync(21);
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await _service.GetTrendingGifsAsync(100);
+            });
         }
     }
 }
