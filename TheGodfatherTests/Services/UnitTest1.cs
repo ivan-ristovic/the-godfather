62f99146bc03be1578f5259fd9533a041785f4bf
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-07-13 20:49:29 +0200

    [Tests] XkcdService

---
+++ TheGodfatherTests/Services/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+using TheGodfather.Services;
+
+namespace TheGodfatherTests.Services
+{
+    [TestClass]
+    public class XkcdServiceTests
+    {
+        [TestMethod]
+        public async Task GetComicAsyncTest()
+        {
+            Assert.IsNotNull(await XkcdService.GetComicAsync());
+            Assert.IsNotNull(await XkcdService.GetComicAsync(1));
+            Assert.IsNotNull(await XkcdService.GetComicAsync(2));
+            Assert.IsNotNull(await XkcdService.GetComicAsync(3));
+            Assert.IsNotNull(await XkcdService.GetComicAsync(4));
+            Assert.IsNotNull(await XkcdService.GetComicAsync(5));
+            Assert.IsNotNull(await XkcdService.GetComicAsync(1000));
+            Assert.IsNotNull(await XkcdService.GetComicAsync(2000));
+
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await XkcdService.GetComicAsync(-1);
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await XkcdService.GetComicAsync(0);
+            });
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => {
+                await XkcdService.GetComicAsync(100000);
+            });
+        }
+
+        [TestMethod]
+        public async Task GetRandomComicAsyncTest()
+        {
+            for (int i = 0; i < 10; i++)
+                Assert.IsNotNull(await XkcdService.GetRandomComicAsync());
+        }
+    }
+}
