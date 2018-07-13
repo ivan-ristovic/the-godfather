5f2bf3a5e6477d899e53cd4e8ff663b7c0c02b68
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-07-13 17:26:55 +0200

    [Tests] QuoteService

---
+++ TheGodfatherTests/Services/QuoteServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using TheGodfather.Services;
+
+namespace TheGodfatherTests.Services
+{
+    [TestClass]
+    public class QuoteServiceTests
+    {
+        [TestMethod]
+        public async Task GetQuoteOfTheDayAsync()
+        {
+            Assert.IsNotNull(await QuoteService.GetQuoteOfTheDayAsync());
+            await Task.Delay(100);
+            Assert.IsNotNull(await QuoteService.GetQuoteOfTheDayAsync(null));
+            await Task.Delay(100);
+            Assert.IsNotNull(await QuoteService.GetQuoteOfTheDayAsync(""));
+            await Task.Delay(100);
+            Assert.IsNotNull(await QuoteService.GetQuoteOfTheDayAsync(" "));
+            await Task.Delay(100);
+            Assert.IsNotNull(await QuoteService.GetQuoteOfTheDayAsync("\n"));
+            await Task.Delay(100);
+
+            Assert.IsNotNull(await QuoteService.GetQuoteOfTheDayAsync("art"));
+            await Task.Delay(100);
+            Assert.IsNotNull(await QuoteService.GetQuoteOfTheDayAsync("inspiring"));
+            await Task.Delay(100);
+            Assert.IsNotNull(await QuoteService.GetQuoteOfTheDayAsync("sports"));
+            await Task.Delay(100);
+            Assert.IsNotNull(await QuoteService.GetQuoteOfTheDayAsync("life"));
+            await Task.Delay(100);
+            Assert.IsNotNull(await QuoteService.GetQuoteOfTheDayAsync("funny"));
+            await Task.Delay(100);
+
+            Assert.IsNull(await QuoteService.GetQuoteOfTheDayAsync("FOOasdnsajdnsjadnj"));
+        }
+
+        [TestMethod]
+        public async Task GetRandomQuoteAsync()
+        {
+            for (int i = 0; i < 10; i++)
+                Assert.IsNotNull(await QuoteService.GetRandomQuoteAsync());
+        }
+    }
+}
