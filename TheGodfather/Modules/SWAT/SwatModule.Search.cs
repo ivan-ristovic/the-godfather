a44d2b14fc84adbe860fc8c102bb434c67b6d6dc
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-07-24 21:50:56 +0200

    [SWAT] Search IP database

--- TheGodfather/Modules/SWAT/SwatModule.Search.cs
+++ TheGodfather/Modules/SWAT/SwatModule.Search.cs
@@ -13,15 +13,16 @@ using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using TheGodfather.Services.Database;
+using System;
 #endregion
 
 namespace TheGodfather.Modules.SWAT
 {
     public partial class SwatModule
     {
-        [Group("banlist"), Module(ModuleType.SWAT)]
-        [Description("SWAT4 banlist manipulation commands.")]
-        [Aliases("b", "blist", "bans", "ban")]
+        [Group("search"), Module(ModuleType.SWAT)]
+        [Description("SWAT4 database search commands.")]
+        [Aliases("s", "find", "lookup")]
         [RequirePrivilegedUser]
         [Hidden]
         public class SwatSearchModule : TheGodfatherModule
@@ -32,10 +33,34 @@ namespace TheGodfather.Modules.SWAT
 
             [GroupCommand]
             public Task ExecuteGroupAsync(CommandContext ctx,
-                                         [Description("Player name to search.")] string name)
+                                         [Description("Player name to search.")] string name,
+                                         [Description("Number of results")] int amount = 10)
             // TODO when IP converter is done, do a check here if the string matches to IP and do an IP search if it does
-                 => SearchNameAsync(ctx, name);
-            
+                 => SearchNameAsync(ctx, name, amount);
+
+
+            #region COMMAND_SEARCH_IP
+            [Command("ip"), Module(ModuleType.SWAT)]
+            [Description("Search for a given IP or range.")]
+            [UsageExamples("!swat search 123.123.123.123")]
+            public async Task SearchIpAsync(CommandContext ctx,
+                                           [Description("IP.")] string ip,
+                                           [Description("Number of results")] int amount = 10)
+            {
+                if (amount < 1 || amount > 100)
+                    throw new ArgumentException("Amount of results to fetch is out of range [1-100].", "amount");
+
+                var res = await Database.SwatDatabaseIpSearchAsync(ip, amount)
+                    .ConfigureAwait(false);
+
+                await ctx.SendCollectionInPagesAsync(
+                    $"Search matches for {ip}",
+                    res,
+                    entry => $"{Formatter.InlineCode(entry.Item2)} | {Formatter.Bold(entry.Item1)}",
+                    DiscordColor.Black
+                ).ConfigureAwait(false);
+            }
+            #endregion
 
             #region COMMAND_SEARCH_NAME
             [Command("name"), Module(ModuleType.SWAT)]
@@ -43,20 +68,21 @@ namespace TheGodfather.Modules.SWAT
             [Aliases("player", "nickname", "nick")]
             [UsageExamples("!swat search EmoPig")]
             public async Task SearchNameAsync(CommandContext ctx,
-                                             [Description("Player name.")] string name)
+                                             [Description("Player name.")] string name,
+                                             [Description("Number of results")] int amount = 10)
             {
+                if (amount < 1 || amount > 100)
+                    throw new ArgumentException("Amount of results to fetch is out of range [1-100].", "amount");
 
-            }
-            #endregion
+                var res = await Database.SwatDatabaseNameSearchAsync(name, amount)
+                    .ConfigureAwait(false);
 
-            #region COMMAND_SEARCH_IP
-            [Command("ip"), Module(ModuleType.SWAT)]
-            [Description("Search for a given IP or range.")]
-            [UsageExamples("!swat search 123.123.123.123")]
-            public async Task SearchIpAsync(CommandContext ctx,
-                                           [Description("IP.")] string ip)
-            {
-
+                await ctx.SendCollectionInPagesAsync(
+                    $"Search matches for {name}",
+                    res,
+                    entry => $"{Formatter.InlineCode(entry.Item2)} | {Formatter.Bold(entry.Item1)}",
+                    DiscordColor.Black
+                ).ConfigureAwait(false);
             }
             #endregion
         }
