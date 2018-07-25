c8dccea9d6e524590857bbc4cde33a562e06686b
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-07-25 17:44:16 +0200

    [Tests] IpGeolocationService

---
+++ TheGodfatherTests/Services/IpGeolocationServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+using TheGodfather.Services;
+using TheGodfather.Services.Common;
+
+namespace TheGodfatherTests.Services
+{
+    [TestClass]
+    public class IpGeolocationServiceTests
+    {
+        [TestMethod]
+        public async Task GetInfoForIpAsyncTest()
+        {
+            IpInfo info;
+
+            info = await IpGeolocationService.GetInfoForIpAsync("208.80.152.201");
+            Assert.IsNotNull(info);
+            Assert.IsTrue(info.Success);
+            Assert.AreEqual("US", info.CountryCode);
+            Assert.AreEqual("San Francisco", info.City);
+            Assert.AreEqual("94105", info.ZipCode);
+            
+            info = await IpGeolocationService.GetInfoForIpAsync("208.80.152.999");
+            Assert.IsNotNull(info);
+            Assert.IsFalse(info.Success);
+
+            await Assert.ThrowsExceptionAsync<ArgumentException>(
+                () => IpGeolocationService.GetInfoForIpAsync(null)
+            );
+            await Assert.ThrowsExceptionAsync<ArgumentException>(
+                () => IpGeolocationService.GetInfoForIpAsync("")
+            );
+            await Assert.ThrowsExceptionAsync<ArgumentException>(
+                () => IpGeolocationService.GetInfoForIpAsync(" ")
+            );
+            await Assert.ThrowsExceptionAsync<ArgumentException>(
+                () => IpGeolocationService.GetInfoForIpAsync("\n")
+            );
+
+            // TODO
+            /*
+            await Assert.ThrowsExceptionAsync<ArgumentException>(
+                () => IpGeolocationService.GetInfoForIpAsync("asdsada")
+            );
+            */
+        }
+    }
+}
