4d1d14aa35710ba7a2379acced199703346063b2
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-08-12 13:57:14 +0200

    [Ratelimit] Add config commands

--- TheGodfather/Modules/Administration/GuildModule.Config.Ratelimit.cs
+++ TheGodfather/Modules/Administration/GuildModule.Config.Ratelimit.cs
@@ -4,15 +4,11 @@ using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 using TheGodfather.Common;
 using TheGodfather.Common.Attributes;
 using TheGodfather.Exceptions;
-using TheGodfather.Extensions;
 using TheGodfather.Modules.Administration.Common;
 using TheGodfather.Modules.Administration.Services;
 using TheGodfather.Services;
@@ -82,7 +78,7 @@ namespace TheGodfather.Modules.Administration
 
                 [GroupCommand, Priority(2)]
                 public Task ExecuteGroupAsync(CommandContext ctx,
-                                             [Description("Enable?")] bool enable ,
+                                             [Description("Enable?")] bool enable,
                                              [Description("Action type.")] PunishmentActionType action,
                                              [Description("Sensitivity (messages per 5s to trigger action).")] short sensitivity = 5)
                     => ExecuteGroupAsync(ctx, enable, sensitivity, action);
@@ -97,12 +93,67 @@ namespace TheGodfather.Modules.Administration
                 {
                     CachedGuildConfig gcfg = this.Shared.GetGuildConfig(ctx.Guild.Id);
                     return InformAsync(ctx, $"Ratelimit watch for this guild is {Formatter.Bold(gcfg.RatelimitEnabled ? "enabled" : "disabled")}", important: false);
-
                 }
 
 
-                // TODO command sensitivity
-                // TODO command action
+                #region COMMAND_RATELIMIT_ACTION
+                [Command("action")]
+                [Description("Set the action to execute when the ratelimit is hit.")]
+                [Aliases("setaction", "a")]
+                [UsageExamples("!guild cfg ratelimit action mute",
+                               "!guild cfg ratelimit action temporaryban")]
+                public async Task SetActionAsync(CommandContext ctx,
+                                                [Description("Action type.")] PunishmentActionType action)
+                {
+                    CachedGuildConfig gcfg = this.Shared.GetGuildConfig(ctx.Guild.Id);
+                    gcfg.RatelimitAction = action;
+
+                    await this.Database.UpdateGuildSettingsAsync(ctx.Guild.Id, gcfg);
+
+                    DiscordChannel logchn = this.Shared.GetLogChannelForGuild(ctx.Client, ctx.Guild);
+                    if (logchn != null) {
+                        var emb = new DiscordEmbedBuilder() {
+                            Title = "Guild config changed",
+                            Color = this.ModuleColor
+                        };
+                        emb.AddField("User responsible", ctx.User.Mention, inline: true);
+                        emb.AddField("Invoked in", ctx.Channel.Mention, inline: true);
+                        emb.AddField("Ratelimit action changed to", gcfg.RatelimitAction.ToTypeString());
+                        await logchn.SendMessageAsync(embed: emb.Build());
+                    }
+
+                    await InformAsync(ctx, $"Ratelimit action for this guild has been changed to {Formatter.Bold(gcfg.RatelimitAction.ToTypeString())}", important: false);
+                }
+                #endregion
+
+                #region COMMAND_RATELIMIT_SENSITIVITY
+                [Command("sensitivity")]
+                [Description("Set the ratelimit sensitivity Ratelimit will be hit if member sends more messages in 5 seconds than given sensitivity number.")]
+                [Aliases("setsensitivity", "setsens", "sens", "s")]
+                [UsageExamples("!guild cfg ratelimit sensitivity 9")]
+                public async Task SetSensitivityAsync(CommandContext ctx,
+                                                     [Description("Action type.")] short sensitivity)
+                {
+                    CachedGuildConfig gcfg = this.Shared.GetGuildConfig(ctx.Guild.Id);
+                    gcfg.RatelimitSensitivity = sensitivity;
+
+                    await this.Database.UpdateGuildSettingsAsync(ctx.Guild.Id, gcfg);
+
+                    DiscordChannel logchn = this.Shared.GetLogChannelForGuild(ctx.Client, ctx.Guild);
+                    if (logchn != null) {
+                        var emb = new DiscordEmbedBuilder() {
+                            Title = "Guild config changed",
+                            Color = this.ModuleColor
+                        };
+                        emb.AddField("User responsible", ctx.User.Mention, inline: true);
+                        emb.AddField("Invoked in", ctx.Channel.Mention, inline: true);
+                        emb.AddField("Ratelimit sensitivity changed to", $"Max {gcfg.RatelimitSensitivity} msgs per 5s");
+                        await logchn.SendMessageAsync(embed: emb.Build());
+                    }
+
+                    await InformAsync(ctx, $"Ratelimit sensitivity for this guild has been changed to {Formatter.Bold(gcfg.RatelimitSensitivity.ToString())} msgs per 5s", important: false);
+                }
+                #endregion
             }
         }
     }
