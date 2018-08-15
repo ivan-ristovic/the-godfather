815b1812586bfdf87df60c40d535aa2c0ba5443e
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-08-15 11:55:07 +0200

    [Launcher] Update w10 launch script

--- launch_win10_x64.ps1
+++ launch_win10_x64.ps1
@@ -1,5 +1,9 @@
+git pull
+
 dotnet publish --self-contained -r win10-x64 -c Release
 
 .\Dependencies\rcedit-x64.exe "TheGodfather\bin\Release\netcoreapp2.1\win10-x64\TheGodfather.exe" --set-icon "TheGodfather\icon.ico"
 
-Start-Process -FilePath ".\TheGodfather\bin\Release\netcoreapp2.1\win10-x64\TheGodfather.exe"
+cd .\TheGodfather\bin\Release\netcoreapp2.1\win10-x64
+
+Start-Process -FilePath ".\TheGodfather.exe"
