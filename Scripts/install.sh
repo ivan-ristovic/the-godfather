e669b04a5b2fcc9e7fa0066ec347a5be443cc237
Ivan Ristovic <ivo.ristovic@gmail.com>
2019-03-10 21:46:29 +0100

    [CI] Test artifacts

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo "Waiting for the bot to shutdown... "
-wait "$1"
+if [ "$#" -e 1 ]; then
+	echo "Waiting for the bot to shutdown... "
+	wait "$1"
+fi
 
-echo "Downloading update... "
+echo "Downloading ... "
 curl -LO "https://ci.appveyor.com/api/projects/ivan-ristovic/the-godfather/artifacts/TheGodfather.zip"
 curl -LO "https://ci.appveyor.com/api/projects/ivan-ristovic/the-godfather/artifacts/TheGodfatherResources.zip"
 
-echo "Extracting update... "
+echo "Extracting ... "
 unzip -o TheGodfather.zip
-unzip -o TheGodfatherResources.zip
+unzip -o TheGodfatherResources.zip ./Resources/
 
 echo "Starting the bot... "
 dotnet TheGodfather.dll
