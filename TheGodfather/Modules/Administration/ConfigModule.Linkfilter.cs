9c40270933c2bc3da22a70b87d545edcf6092325
Ivan Ristović <ivo.ristovic@gmail.com>
2020-11-21 22:06:49 +0100

    [Administration] Refactor linkfilter module

---
+++ TheGodfather/Modules/Administration/ConfigModule.Linkfilter.cs
@@ -0,0 +1,183 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using TheGodfather.Exceptions;
+using TheGodfather.Extensions;
+using TheGodfather.Modules.Administration.Common;
+using TheGodfather.Modules.Administration.Extensions;
+using TheGodfather.Modules.Administration.Services;
+using TheGodfather.Services.Common;
+
+namespace TheGodfather.Modules.Administration
+{
+    public partial class ConfigModule
+    {
+        [Group("linkfilter")]
+        [Aliases("lf")]
+        public class LinkfilterModule : TheGodfatherServiceModule<LinkfilterService>
+        {
+            public LinkfilterModule(LinkfilterService service)
+                : base(service) { }
+
+
+            #region config linkfilter
+            [GroupCommand, Priority(1)]
+            public async Task ExecuteGroupAsync(CommandContext ctx,
+                                               [Description("desc-enable")] bool enable)
+            {
+                await ctx.Services.GetRequiredService<GuildConfigService>().ModifyConfigAsync(ctx.Guild.Id, cfg => {
+                    cfg.LinkfilterEnabled = enable;
+                });
+                await ctx.InfoAsync();
+                await this.LogConfigChangeAsync(ctx, "str-lf", enable);
+            }
+
+            [GroupCommand, Priority(0)]
+            public Task ExecuteGroupAsync(CommandContext ctx)
+            {
+                LinkfilterSettings settings = ctx.Services.GetRequiredService<GuildConfigService>().GetCachedConfig(ctx.Guild.Id).LinkfilterSettings;
+                if (!settings.Enabled)
+                    throw new CommandFailedException(ctx, "cmd-err-lf-off");
+
+                return ctx.RespondWithLocalizedEmbedAsync(emb => {
+                    emb.WithLocalizedTitle("str-lf");
+                    emb.WithColor(this.ModuleColor);
+                    emb.AddField("str-lf-invite", settings.BlockDiscordInvites ? "str-on" : "str-off", inline: true);
+                    emb.AddField("str-lf-ddos", settings.BlockBooterWebsites ? "str-on" : "str-off", inline: true);
+                    emb.AddField("str-lf-gore", settings.BlockDisturbingWebsites ? "str-on" : "str-off", inline: true);
+                    emb.AddField("str-lf-ip", settings.BlockIpLoggingWebsites ? "str-on" : "str-off", inline: true);
+                    emb.AddField("str-lf-urlshort", settings.BlockDisturbingWebsites ? "str-on" : "str-off", inline: true);
+                });
+            }
+            #endregion
+
+            #region config linkfilter booters
+            [Command("booters"), Priority(1)]
+            [Aliases("ddos", "boot", "dos")]
+            public async Task BootersAsync(CommandContext ctx,
+                                          [Description("desc-enable")] bool enable)
+            {
+                await ctx.Services.GetRequiredService<GuildConfigService>().ModifyConfigAsync(ctx.Guild.Id, cfg => {
+                    cfg.LinkfilterBootersEnabled = enable;
+                });
+                await ctx.InfoAsync();
+                await this.LogConfigChangeAsync(ctx, "str-lf-ddos", enable);
+            }
+
+            [Command("booters"), Priority(0)]
+#pragma warning disable CA1822 // Mark members as static
+            public Task BootersAsync(CommandContext ctx)
+#pragma warning restore CA1822 // Mark members as static
+            {
+                CachedGuildConfig gcfg = ctx.Services.GetRequiredService<GuildConfigService>().GetCachedConfig(ctx.Guild.Id);
+                return ctx.InfoAsync("fmt-lf-ddos", gcfg.LinkfilterSettings.BlockBooterWebsites);
+            }
+            #endregion
+
+            #region config linkfilter invites
+            [Command("invites"), Priority(1)]
+            [Aliases("invite", "inv", "i")]
+            public async Task InvitesAsync(CommandContext ctx,
+                                          [Description("desc-enable")] bool enable)
+            {
+                await ctx.Services.GetRequiredService<GuildConfigService>().ModifyConfigAsync(ctx.Guild.Id, cfg => {
+                    cfg.LinkfilterDiscordInvitesEnabled = enable;
+                });
+                await ctx.InfoAsync();
+                await this.LogConfigChangeAsync(ctx, "str-lf-invite", enable);
+            }
+
+            [Command("invites"), Priority(0)]
+#pragma warning disable CA1822 // Mark members as static
+            public Task InvitesAsync(CommandContext ctx)
+#pragma warning restore CA1822 // Mark members as static
+            {
+                CachedGuildConfig gcfg = ctx.Services.GetRequiredService<GuildConfigService>().GetCachedConfig(ctx.Guild.Id);
+                return ctx.InfoAsync("fmt-lf-invite", gcfg.LinkfilterSettings.BlockDiscordInvites);
+            }
+            #endregion
+
+            #region config linkfilter shocksites
+            [Command("shocksites"), Priority(1)]
+            [Aliases("disturbingsites", "shock", "disturbing", "gore")]
+            public async Task DisturbingSitesAsync(CommandContext ctx,
+                                                  [Description("desc-enable")] bool enable)
+            {
+                await ctx.Services.GetRequiredService<GuildConfigService>().ModifyConfigAsync(ctx.Guild.Id, cfg => {
+                    cfg.LinkfilterDisturbingWebsitesEnabled = enable;
+                });
+                await ctx.InfoAsync();
+                await this.LogConfigChangeAsync(ctx, "str-lf-gore", enable);
+            }
+
+            [Command("disturbingsites"), Priority(0)]
+#pragma warning disable CA1822 // Mark members as static
+            public Task DisturbingSitesAsync(CommandContext ctx)
+#pragma warning restore CA1822 // Mark members as static
+            {
+                CachedGuildConfig gcfg = ctx.Services.GetRequiredService<GuildConfigService>().GetCachedConfig(ctx.Guild.Id);
+                return ctx.InfoAsync("fmt-lf-gore", gcfg.LinkfilterSettings.BlockDisturbingWebsites);
+            }
+            #endregion
+
+            #region config linkfilter iploggers
+            [Command("iploggers"), Priority(1)]
+            [Aliases("ip", "loggers")]
+            public async Task IpLoggersAsync(CommandContext ctx,
+                                            [Description("desc-enable")] bool enable)
+            {
+                await ctx.Services.GetRequiredService<GuildConfigService>().ModifyConfigAsync(ctx.Guild.Id, cfg => {
+                    cfg.LinkfilterIpLoggersEnabled = enable;
+                });
+                await ctx.InfoAsync();
+                await this.LogConfigChangeAsync(ctx, "str-lf-ip", enable);
+            }
+
+            [Command("iploggers"), Priority(0)]
+#pragma warning disable CA1822 // Mark members as static
+            public Task IpLoggersAsync(CommandContext ctx)
+#pragma warning restore CA1822 // Mark members as static
+            {
+                CachedGuildConfig gcfg = ctx.Services.GetRequiredService<GuildConfigService>().GetCachedConfig(ctx.Guild.Id);
+                return ctx.InfoAsync("fmt-lf-ip", gcfg.LinkfilterSettings.BlockIpLoggingWebsites);
+            }
+            #endregion
+
+            #region config linkfilter shorteners
+            [Command("shorteners"), Priority(1)]
+            [Aliases("urlshort", "shortenurl", "urlshorteners")]
+            public async Task ShortenersAsync(CommandContext ctx,
+                                             [Description("desc-enable")] bool enable)
+            {
+                await ctx.Services.GetRequiredService<GuildConfigService>().ModifyConfigAsync(ctx.Guild.Id, cfg => {
+                    cfg.LinkfilterUrlShortenersEnabled = enable;
+                });
+                await ctx.InfoAsync();
+                await this.LogConfigChangeAsync(ctx, "str-lf-urlshort", enable);
+            }
+
+            [Command("shorteners"), Priority(0)]
+#pragma warning disable CA1822 // Mark members as static
+            public Task ShortenersAsync(CommandContext ctx)
+#pragma warning restore CA1822 // Mark members as static
+            {
+                CachedGuildConfig gcfg = ctx.Services.GetRequiredService<GuildConfigService>().GetCachedConfig(ctx.Guild.Id);
+                return ctx.InfoAsync("fmt-lf-urlshort", gcfg.LinkfilterSettings.BlockUrlShorteners);
+            }
+            #endregion
+
+
+            #region Helpers
+            protected Task LogConfigChangeAsync(CommandContext ctx, string key, bool value)
+            {
+                return ctx.GuildLogAsync(emb => {
+                    emb.WithLocalizedTitle("evt-cfg-upd");
+                    emb.WithColor(this.ModuleColor);
+                    emb.AddLocalizedField(key, value ? "str-on" : "str-off");
+                });
+            }
+            #endregion
+        }
+    }
+}
