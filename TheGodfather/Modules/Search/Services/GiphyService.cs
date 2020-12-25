d9189857131b3eb60ab3038324305fc525c2c244
Ivan Ristović <ivo.ristovic@gmail.com>
2020-12-25 14:13:31 +0100

    [Searches] Refactor GIPHY module

--- TheGodfather/Modules/Search/Services/GiphyService.cs
+++ TheGodfather/Modules/Search/Services/GiphyService.cs
@@ -1,6 +1,4 @@
-﻿#region USING_DIRECTIVES
-using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using GiphyDotNet.Manager;
 using GiphyDotNet.Model.Parameters;
 using TheGodfather.Services;
@@ -8,15 +6,14 @@ using ImageData = GiphyDotNet.Model.GiphyImage.Data;
 using RandomImageData = GiphyDotNet.Model.GiphyRandomImage.Data;
 using RandomImageResult = GiphyDotNet.Model.Results.GiphyRandomResult;
 using SearchResult = GiphyDotNet.Model.Results.GiphySearchResult;
-#endregion
 
 namespace TheGodfather.Modules.Search.Services
 {
-    public class GiphyService : ITheGodfatherService
+    public sealed class GiphyService : ITheGodfatherService
     {
         public bool IsDisabled => this.giphy is null;
 
-        private readonly Giphy giphy;
+        private readonly Giphy? giphy;
 
 
         public GiphyService(BotConfigService cfg)
@@ -26,18 +23,15 @@ namespace TheGodfather.Modules.Search.Services
         }
 
 
-        public async Task<ImageData[]> SearchAsync(string query, int amount = 1)
+        public async Task<ImageData[]?> SearchGifAsync(string query, int amount = 1)
         {
             if (this.IsDisabled)
                 return null;
 
-            if (string.IsNullOrWhiteSpace(query))
-                throw new ArgumentException("Query missing!", nameof(query));
-
             if (amount < 1 || amount > 20)
-                throw new ArgumentException("Result amount out of range (max 20)", nameof(amount));
+                amount = 1;
 
-            SearchResult res = await this.giphy.GifSearch(new SearchParameter {
+            SearchResult res = await this.giphy!.GifSearch(new SearchParameter {
                 Query = query,
                 Limit = amount
             }).ConfigureAwait(false);
@@ -45,24 +39,64 @@ namespace TheGodfather.Modules.Search.Services
             return res.Data;
         }
 
-        public async Task<RandomImageData> GetRandomGifAsync()
+        public async Task<RandomImageData?> GetRandomGifAsync()
         {
             if (this.IsDisabled)
                 return null;
 
-            RandomImageResult res = await this.giphy.RandomGif(new RandomParameter()).ConfigureAwait(false);
+            RandomImageResult res = await this.giphy!.RandomGif(new RandomParameter()).ConfigureAwait(false);
             return res?.Data;
         }
 
-        public async Task<ImageData[]> GetTrendingGifsAsync(int amount = 1)
+        public async Task<ImageData[]?> GetTrendingGifsAsync(int amount = 1)
         {
             if (this.IsDisabled)
                 return null;
 
             if (amount < 1 || amount > 20)
-                throw new ArgumentException("Result amount out of range (max 20)", nameof(amount));
+                amount = 1;
 
-            SearchResult res = await this.giphy.TrendingGifs(new TrendingParameter {
+            SearchResult res = await this.giphy!.TrendingGifs(new TrendingParameter {
+                Limit = amount
+            }).ConfigureAwait(false);
+
+            return res.Data;
+        }
+
+        public async Task<ImageData[]?> SearchStickerAsync(string query, int amount = 1)
+        {
+            if (this.IsDisabled)
+                return null;
+
+            if (amount < 1 || amount > 20)
+                amount = 1;
+
+            SearchResult res = await this.giphy!.StickerSearch(new SearchParameter {
+                Query = query,
+                Limit = amount
+            }).ConfigureAwait(false);
+
+            return res.Data;
+        }
+
+        public async Task<RandomImageData?> GetRandomStickerAsync()
+        {
+            if (this.IsDisabled)
+                return null;
+
+            RandomImageResult res = await this.giphy!.RandomSticker(new RandomParameter()).ConfigureAwait(false);
+            return res?.Data;
+        }
+
+        public async Task<ImageData[]?> GetTrendingStickerssAsync(int amount = 1)
+        {
+            if (this.IsDisabled)
+                return null;
+
+            if (amount < 1 || amount > 20)
+                amount = 1;
+
+            SearchResult res = await this.giphy!.TrendingStickers(new TrendingParameter {
                 Limit = amount
             }).ConfigureAwait(false);
 
