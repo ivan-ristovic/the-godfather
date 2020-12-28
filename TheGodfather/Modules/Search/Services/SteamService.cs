13681a1431429079ec585636bebde3fd8bf9f32f
Ivan Ristović <ivo.ristovic@gmail.com>
2020-12-28 17:05:22 +0100

    [Searches] Refactor steam service

--- TheGodfather/Modules/Search/Services/SteamService.cs
+++ TheGodfather/Modules/Search/Services/SteamService.cs
@@ -1,104 +1,70 @@
-﻿#region USING_DIRECTIVES
+﻿using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using DSharpPlus;
-using DSharpPlus.Entities;
 using Steam.Models.SteamCommunity;
 using SteamWebAPI2.Interfaces;
 using SteamWebAPI2.Utilities;
 using TheGodfather.Services;
-#endregion
 
 namespace TheGodfather.Modules.Search.Services
 {
-    public class SteamService : ITheGodfatherService
+    public sealed class SteamService : ITheGodfatherService
     {
+        private const string SteamCommunityURL = "http://steamcommunity.com";
+        private static readonly Regex _tagRegex = new Regex(@"<[^>]*>", RegexOptions.Compiled);
+
         public bool IsDisabled => this.user is null;
 
-        private readonly SteamUser user;
-
-
-        public static string GetProfileUrlForId(ulong id)
-            => $"http://steamcommunity.com/id/{ id }/";
+        private readonly SteamUser? user;
 
 
         public SteamService(BotConfigService cfg)
         {
-            // FIXME
-            //if (!string.IsNullOrWhiteSpace(cfg.CurrentConfiguration.SteamKey))
-            //    this.user = new SteamUser(cfg.CurrentConfiguration.SteamKey);
+            if (!string.IsNullOrWhiteSpace(cfg.CurrentConfiguration.SteamKey)) {
+                var webInterfaceFactory = new SteamWebInterfaceFactory(cfg.CurrentConfiguration.SteamKey);
+                this.user = webInterfaceFactory.CreateSteamWebInterface<SteamUser>();
+            }
         }
 
 
-        public DiscordEmbed EmbedSteamResult(SteamCommunityProfileModel model, PlayerSummaryModel summary)
+        public string GetCommunityProfileUrl(ulong id)
+            => $"{SteamCommunityURL}/id/{id}";
+
+        public string GetCommunityProfileUrl(string username)
+            => $"{SteamCommunityURL}/id/{WebUtility.UrlEncode(username)}";
+
+        public async Task<(SteamCommunityProfileModel, PlayerSummaryModel)?> GetInfoAsync(string vanityUrl)
         {
             if (this.IsDisabled)
                 return null;
-
-            var em = new DiscordEmbedBuilder {
-                Title = summary.Nickname,
-                Description = Regex.Replace(model.Summary, "<[^>]*>", string.Empty),
-                Thumbnail = new DiscordEmbedBuilder.EmbedThumbnail {
-                    Url = model.AvatarFull.ToString()
-                },
-                Color = DiscordColor.Black,
-                Url = GetProfileUrlForId(model.SteamID)
-            };
-
-            if (summary.ProfileVisibility != ProfileVisibility.Public) {
-                em.Description = "This profile is private.";
-                return em;
-            }
-
-            em.AddField("Member since", summary.AccountCreatedDate.ToUniversalTime().ToString(), inline: true);
-
-            if (summary.UserStatus != Steam.Models.SteamCommunity.UserStatus.Offline)
-                em.AddField("Status:", summary.UserStatus.ToString(), inline: true);
-            else
-                em.AddField("Last seen:", summary.LastLoggedOffDate.ToUniversalTime().ToString(), inline: true);
-
-            if (!string.IsNullOrWhiteSpace(summary.PlayingGameName))
-                em.AddField("Playing: ", summary.PlayingGameName);
-
-            if (!string.IsNullOrWhiteSpace(model.Location))
-                em.AddField("Location: ", model.Location);
-
-            // em.AddField("Game activity", $"{model.HoursPlayedLastTwoWeeks} hours past 2 weeks.", inline: true);
-
-            if (model.IsVacBanned) {
-                System.Collections.Generic.IReadOnlyCollection<PlayerBansModel> bans = this.user.GetPlayerBansAsync(model.SteamID).Result.Data;
-
-                uint bancount = 0;
-                foreach (PlayerBansModel b in bans)
-                    bancount += b.NumberOfVACBans;
-
-                em.AddField("VAC Status:", $"{Formatter.Bold(bancount.ToString())} ban(s) on record.", inline: true);
-            } else {
-                em.AddField("VAC Status:", "No bans registered");
-            }
-
-            return em.Build();
-        }
-
-        public async Task<DiscordEmbed> GetEmbeddedInfoAsync(ulong id)
-        {
-            if (this.IsDisabled)
-                return null;
-
-            SteamCommunityProfileModel profile = null;
-            ISteamWebResponse<PlayerSummaryModel> summary = null;
 
             try {
-                profile = await this.user.GetCommunityProfileAsync(id);
-                summary = await this.user.GetPlayerSummaryAsync(id);
+                ISteamWebResponse<ulong> res = await this.user!.ResolveVanityUrlAsync(vanityUrl);
+                return await this.GetInfoAsync(res.Data);
             } catch {
+                return null;
+            }
+        }
 
+        public async Task<(SteamCommunityProfileModel, PlayerSummaryModel)?> GetInfoAsync(ulong id)
+        {
+            if (this.IsDisabled)
+                return null;
+
+            SteamCommunityProfileModel? profile;
+            ISteamWebResponse<PlayerSummaryModel>? summary;
+            try {
+                profile = await this.user!.GetCommunityProfileAsync(id);
+                summary = await this.user!.GetPlayerSummaryAsync(id);
+            } catch {
+                return null;
             }
 
             if (profile is null || summary is null || summary.Data is null)
                 return null;
 
-            return this.EmbedSteamResult(profile, summary.Data);
+            profile.Summary = _tagRegex.Replace(profile.Summary, string.Empty);
+            return (profile, summary.Data);
         }
     }
 }
--- TheGodfather/Modules/Search/SteamModule.cs
+++ TheGodfather/Modules/Search/SteamModule.cs
@@ -1,42 +1,102 @@
-﻿#region USING_DIRECTIVES
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
+using Humanizer;
+using Steam.Models.SteamCommunity;
 using TheGodfather.Attributes;
 using TheGodfather.Exceptions;
+using TheGodfather.Extensions;
 using TheGodfather.Modules.Search.Services;
-#endregion
 
 namespace TheGodfather.Modules.Search
 {
     [Group("steam"), Module(ModuleType.Searches), NotBlocked]
-    [Description("Steam commands. Group call searches steam profiles for a given ID.")]
     [Aliases("s", "st")]
-
     [Cooldown(3, 5, CooldownBucketType.Channel)]
-    public class SteamModule : TheGodfatherServiceModule<SteamService>
+    public sealed class SteamModule : TheGodfatherServiceModule<SteamService>
     {
+        #region steam
+        [GroupCommand, Priority(1)]
+        public Task ExecuteGroupAsync(CommandContext ctx,
+                                     [Description("desc-id")] ulong id)
+            => this.InfoAsync(ctx, id);
 
-        #region COMMAND_STEAM_PROFILE
-        [Command("profile")]
-        [Description("Get Steam user information for user based on his ID.")]
-        [Aliases("id", "user")]
+        [GroupCommand, Priority(0)]
+        public Task ExecuteGroupAsync(CommandContext ctx,
+                                     [RemainingText, Description("desc-username")] string username)
+            => this.InfoAsync(ctx, username);
+        #endregion
 
+        #region steam profile
+        [Command("profile"), Priority(1)]
+        [Aliases("id", "user", "info")]
         public async Task InfoAsync(CommandContext ctx,
-                                   [Description("ID.")] ulong id)
+                                   [Description("desc-id")] ulong id)
         {
             if (this.Service.IsDisabled)
                 throw new ServiceDisabledException(ctx);
 
-            DiscordEmbed em = await this.Service.GetEmbeddedInfoAsync(id);
-            if (em is null) {
-                await this.InformFailureAsync(ctx, "User with such ID does not exist!");
-                return;
-            }
+            await this.PrintProfileAsync(ctx, await this.Service.GetInfoAsync(id));
+        }
 
-            await ctx.RespondAsync(embed: em);
+        [Command("profile"), Priority(0)]
+        public async Task InfoAsync(CommandContext ctx,
+                                  [Description("desc-username")] string username)
+        {
+            if (this.Service.IsDisabled)
+                throw new ServiceDisabledException(ctx);
+
+            await this.PrintProfileAsync(ctx, await this.Service.GetInfoAsync(username));
+        }
+        #endregion
+
+
+        #region internals
+        private Task PrintProfileAsync(CommandContext ctx, (SteamCommunityProfileModel, PlayerSummaryModel)? res)
+        {
+            if (res is null)
+                throw new CommandFailedException(ctx, "cmd-err-steam");
+
+            (SteamCommunityProfileModel model, PlayerSummaryModel summary) = res.Value;
+            return ctx.RespondWithLocalizedEmbedAsync(emb => {
+                emb.WithTitle(summary.Nickname);
+                emb.WithDescription(model.Summary);
+                emb.WithColor(this.ModuleColor);
+                emb.WithThumbnail(model.AvatarMedium.ToString());
+                emb.WithUrl(this.Service.GetCommunityProfileUrl(model.SteamID));
+
+                if (summary.ProfileVisibility != ProfileVisibility.Public) {
+                    emb.WithLocalizedDescription("str-profile-private");
+                    return;
+                }
+
+                emb.AddLocalizedTimestampField("str-member-since", summary.AccountCreatedDate, inline: true);
+
+                if (summary.UserStatus != UserStatus.Offline)
+                    emb.AddLocalizedTitleField("str-status", summary.UserStatus.Humanize(LetterCasing.Sentence), inline: true);
+                else
+                    emb.AddLocalizedTimestampField("str-last-seen", summary.LastLoggedOffDate, inline: true);
+
+                emb.AddLocalizedTitleField("str-playing ", summary.PlayingGameName, inline: true, unknown: false);
+                emb.AddLocalizedTitleField("str-location ", model.Location, inline: true, unknown: false);
+
+                // TODO add more
+
+                // emb.AddField("Game activity", $"{model.HoursPlayedLastTwoWeeks} hours past 2 weeks.", inline: true);
+
+                //if (model.IsVacBanned) {
+                //    System.Collections.Generic.IReadOnlyCollection<PlayerBansModel> bans = this.user.GetPlayerBansAsync(model.SteamID).Result.Data;
+
+                //    uint bancount = 0;
+                //    foreach (PlayerBansModel b in bans)
+                //        bancount += b.NumberOfVACBans;
+
+                //    em.AddField("VAC Status:", $"{Formatter.Bold(bancount.ToString())} ban(s) on record.", inline: true);
+                //} else {
+                //    em.AddField("VAC Status:", "No bans registered");
+                //}
+            });
         }
         #endregion
     }
