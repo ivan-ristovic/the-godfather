c4a5b578a2d0adba464778681c38ba607de1a02a
Ivan Ristović <ivo.ristovic@gmail.com>
2021-01-21 16:27:05 +0100

    [Currency] Make work module take time of day into consideration

--- TheGodfather/Modules/Currency/Services/WorkService.cs
+++ TheGodfather/Modules/Currency/Services/WorkService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using TheGodfather.Common;
 using TheGodfather.Modules.Currency.Common;
 using TheGodfather.Services;
@@ -29,16 +30,22 @@ namespace TheGodfather.Modules.Currency.Services
         public async Task<string> WorkAsync(ulong gid, ulong uid)
         {
             RegularWorkType workType = this.rng.ChooseRandomEnumValue<RegularWorkType>();
-            int earned = this.rng.Next(WorkEarnLimit) + 1;
+            TimeOfDay tod = this.FindCurrentTimeOfDay(gid);
+            int earned = tod switch {
+                TimeOfDay.Evening => (int)((this.rng.Next(WorkEarnLimit) + 1) * 1.5),
+                TimeOfDay.Night => (int)((this.rng.Next(WorkEarnLimit) + 1) * 0.5),
+                _ => this.rng.Next(WorkEarnLimit) + 1,
+            };
             await this.bas.IncreaseBankAccountAsync(gid, uid, earned);
-            return this.lcs.GetString(gid, $"fmt-work-{(int)workType}", earned);
+            return this.lcs.GetString(gid, $"fmt-work-{(int)workType}-{(int)tod}", earned);
         }
 
         public async Task<string> CrimeAsync(ulong gid, ulong uid)
         {
             CrimeWorkType crimeType = this.rng.ChooseRandomEnumValue<CrimeWorkType>();
+            TimeOfDay tod = this.FindCurrentTimeOfDay(gid);
             int change = this.rng.Next(CrimeEarnLimit) + 1;
-            if (this.rng.NextBool(9)) {
+            if (this.rng.NextBool(9 - (int)tod * 2)) {
                 await this.bas.ModifyBankAccountAsync(gid, uid, v => v - change);
                 return this.lcs.GetString(gid, $"fmt-work-crime-fail-{(int)crimeType}", change);
             } else {
@@ -50,7 +57,8 @@ namespace TheGodfather.Modules.Currency.Services
         public async Task<string> StreetsAsync(ulong gid, ulong uid)
         {
             int change = this.rng.Next(StreetEarnLimit) + 1;
-            if (this.rng.NextBool()) {
+            TimeOfDay tod = this.FindCurrentTimeOfDay(gid);
+            if (this.rng.NextBool(tod == TimeOfDay.Evening ? 1 : 2)) {
                 NegativeStreetWorkType streetType = this.rng.ChooseRandomEnumValue<NegativeStreetWorkType>();
                 await this.bas.ModifyBankAccountAsync(gid, uid, v => v - change);
                 return this.lcs.GetString(gid, $"fmt-work-streets-fail-{(int)streetType}", change);
@@ -59,6 +67,25 @@ namespace TheGodfather.Modules.Currency.Services
                 await this.bas.IncreaseBankAccountAsync(gid, uid, change);
                 return this.lcs.GetString(gid, $"fmt-work-streets-{(int)streetType}", change);
             }
+        }
+
+
+        private TimeOfDay FindCurrentTimeOfDay(ulong gid)
+        {
+            DateTimeOffset now = lcs.GetLocalizedTime(gid);
+            return now.LocalDateTime.TimeOfDay.Hours switch {
+                < 7 => TimeOfDay.Night,
+                < 17 => TimeOfDay.WorkHours,
+                _ => TimeOfDay.Evening,
+            };
+        }
+
+
+        private enum TimeOfDay
+        {
+            WorkHours = 0, 
+            Evening = 1,
+            Night = 2,
         }
     }
 }
