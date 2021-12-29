e379f803ae4e000591ef8afdcd504e4bc281d0f7
ivan-ristovic <ivan.ristovic95@gmail.com>
2021-12-29 18:08:18 +0100

    [Docker] Compose stack added

--- launch.sh
+++ launch.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-# docker pull ivan-ristovic/the-godfather:latest
-docker run -v $PWD/data:/app/data the-godfather:latest
+set -xe;
+
+if [[ ! -f .env ]]; then
+    echo 'Cannot find .env file, re-creating ...'
+    cp .env_example .env
+    echo 'Attempting to edit generated env file with $EDITOR ...'
+    $EDITOR .env
+fi
+
+mkdir -p data
+mkdir -p data/backup
+mkdir -p data/logs
+mkdir -p data/lavalink
+
+if [[ ! -f data/lavalink/application.yml ]]; then
+    echo 'Warning: lavalink config not found'
+fi
+
+echo 'Starting the service stack ...'
+sleep 1
+docker-compose up -d --remove-orphans
+
+source log_stream.sh
 
